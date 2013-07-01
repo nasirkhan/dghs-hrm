63ceaa8c445804a0f603e94c0c183d703e63f080
Nasir Khan Saikat <nasir8891@gmail.com>
2013-07-01 16:37:31 +0600

    new function sets

--- include/functions_app_specific.php
+++ include/functions_app_specific.php
@@ -5,16 +5,16 @@
  * @param type $org_code
  * @return string org_type_name
  */
-function getOrgTypeNameFormOrgCode($org_code){
-    $sql = "SELECT org_code, org_name, org_type_code, organization_id FROM organization WHERE org_code = $org_code  LIMIT 1";    
+function getOrgTypeNameFormOrgCode($org_code) {
+    $sql = "SELECT org_code, org_name, org_type_code, organization_id FROM organization WHERE org_code = $org_code  LIMIT 1";
     $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>getOrgTypeNameFormOrgCode:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
-    
-    $data = mysql_fetch_assoc($result);    
+
+    $data = mysql_fetch_assoc($result);
     $org_type_code = $data['org_type_code'];
-    
-    $sql = "SELECT org_type_id, org_type_code, org_type_name FROM org_type WHERE org_type_id = $org_type_code LIMIT 1";    
+
+    $sql = "SELECT org_type_id, org_type_code, org_type_name FROM org_type WHERE org_type_id = $org_type_code LIMIT 1";
     $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>getOrgTypeNameFormOrgCode:2</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
-    
+
     $org_type_data = mysql_fetch_assoc($result);
     $org_type_name = $org_type_data['org_type_name'];
     return $org_type_name;
@@ -25,10 +25,10 @@ function getOrgTypeNameFormOrgCode($org_code){
  * @param type $org_type_id
  * @return string org_type_name
  */
-function getOrgTypeNameFormOrgTypeId($org_type_id){  
-    $sql = "SELECT org_type_id, org_type_code, org_type_name FROM org_type WHERE org_type_id = $org_type_id LIMIT 1";    
+function getOrgTypeNameFormOrgTypeId($org_type_id) {
+    $sql = "SELECT org_type_id, org_type_code, org_type_name FROM org_type WHERE org_type_id = $org_type_id LIMIT 1";
     $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>getOrgTypeNameFormOrgTypeId:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
-    
+
     $org_type_data = mysql_fetch_assoc($result);
     $org_type_name = $org_type_data['org_type_name'];
     return $org_type_name;
@@ -39,25 +39,56 @@ function getOrgTypeNameFormOrgTypeId($org_type_id){
  * @param type $agency_code
  * @return type
  */
-function getAgencyNameFromAgencyCode($agency_code){
-    $sql = "SELECT org_agency_code_name FROM org_agency_code WHERE org_agency_code = $agency_code LIMIT 1";    
+function getAgencyNameFromAgencyCode($agency_code) {
+    $sql = "SELECT org_agency_code_name FROM org_agency_code WHERE org_agency_code = $agency_code LIMIT 1";
     $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>getAgencyNameFromAgencyCode:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
-    
+
     $org_data = mysql_fetch_assoc($result);
     $org_agency_code_name = $org_data['org_agency_code_name'];
     return $org_agency_code_name;
 }
 
 /**
- * 
- * @param type $functional_code
- * @return type
+ * Get <b>Functional Name</b> form the <b>Functional Code</b>
+ * @param Int $functional_code
+ * @return String
  */
-function getFunctionalNameFromFunctionalCode($functional_code){
-    $sql = "SELECT org_organizational_functions_name FROM org_organizational_functions WHERE org_organizational_functions_code = $functional_code LIMIT 1";    
+function getFunctionalNameFromFunctionalCode($functional_code) {
+    $sql = "SELECT org_organizational_functions_name FROM org_organizational_functions WHERE org_organizational_functions_code = $functional_code LIMIT 1";
     $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>getFunctionalNameFromFunctionalCode:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
-    
+
     $org_data = mysql_fetch_assoc($result);
     return $org_data['org_organizational_functions_name'];
 }
+
+/**
+ * Get the <b>Organization Name</b> from the <b>Organization Code</b><b></b>
+ * @param int $org_code Organization Code
+ * @return String org_name Organization Name
+ */
+function getOrgNameFormOrgCode($org_code) {
+    $sql = "SELECT organization.id,organization.org_name FROM organization WHERE organization.org_code = $org_code";
+    $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>getOrgNameFormOrgCode:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+
+    $org_data = mysql_fetch_assoc($result);
+    return $org_data['org_name'];
+}
+
+/**
+ * Get the <b>Organization Name</b> and <b>Organization Type Code</b>from the <b>Organization Code</b><b></b>
+ * @param int $org_code Organization Code
+ * @return String <b>Organization Name(org_name)</b> and <b>Organization Type Code (org_type_code)</b>
+ */
+function getOrgNameAndOrgTypeCodeFormOrgCode($org_code) {
+    $sql = "SELECT organization.org_name,organization.org_type_code FROM organization WHERE organization.org_code = $org_code";
+    $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>getOrgNameFormOrgCode:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+
+    $org_data = mysql_fetch_assoc($result);
+
+    $data['org_name'] = $org_data['org_name'];
+    $data['org_type_code'] = $org_data['org_type_code'];
+
+    return $data;
+}
+
 ?>
