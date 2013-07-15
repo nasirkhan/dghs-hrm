69ac80da32c4a4ac13e848de30ab6d924b8ba9f8
nasirkhan <nasir8891@gmail.com>
2013-07-15 16:20:47 +0600

    update

--- get/get_division_name.php
+++ get/get_division_name.php
@@ -1,14 +1,14 @@
 <?php
 require_once '../configuration.php';
 
-$sql = "SELECT `org_agency_code`, `org_agency_name` FROM `org_agency_code` ORDER BY org_agency_name";
+$sql = "SELECT `division_name`, `division_bbs_code` FROM `admin_division` ORDER BY division_name";
 $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>checkPasswordIsCorrect:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
 
 $data = array();
 while ($row = mysql_fetch_array($result)) {
     $data[] = array(
-        'text' => $row['org_agency_name'],
-        'value' => $row['org_agency_code']
+        'text' => $row['division_name'],
+        'value' => $row['division_bbs_code']
     );
 }
 $json_data = json_encode($data);
