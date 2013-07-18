52c8dee2aa7d4457109589f937a7a819af676576
nasirkhan <nasir8891@gmail.com>
2013-07-18 18:04:18 +0600

    move resquest

---
+++ post/post_move_request.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../configuration.php';
+
+$staff_id = mysql_real_escape_string($_POST['staff_id']);
+$sanctioned_post = mysql_real_escape_string($_POST['sanctioned_post']);
+$new_sanctioned_post = mysql_real_escape_string($_POST['new_sanctioned_post']);
+$govt_order = mysql_real_escape_string($_POST['govt_order']);
+$comment = mysql_real_escape_string($_POST['comment']);
+?>
