6d04303bb17858560ea93be67c00b2893a8d851d
nasirkhan <nasir8891@gmail.com>
2013-07-20 04:25:32 +0600

    new sanctioned post page

---
+++ get/get_sp_designations.php
@@ -0,0 +1,75 @@
+<?php
+require_once '../configuration.php';
+
+if (isset($_REQUEST["first_level_id"])) {
+    $first_level_id = mysql_real_escape_string($_REQUEST['first_level_id']);
+}
+if (isset($_REQUEST["second_level_id"])) {
+    $second_level_id = mysql_real_escape_string($_REQUEST['second_level_id']);
+}
+if (isset($_REQUEST["div"])) {
+    $div_id = mysql_real_escape_string($_REQUEST['div']);
+}
+if (isset($_REQUEST["loading"])) {
+    $loading_id = mysql_real_escape_string($_REQUEST['loading']);
+}
+
+$org_code = $_SESSION['org_code'];
+
+$sql = "SELECT
+total_manpower_imported_sanctioned_post_copy.id,
+total_manpower_imported_sanctioned_post_copy.designation,
+total_manpower_imported_sanctioned_post_copy.designation_code,
+total_manpower_imported_sanctioned_post_copy.staff_id
+FROM total_manpower_imported_sanctioned_post_copy
+WHERE
+total_manpower_imported_sanctioned_post_copy.first_level_id = $first_level_id AND
+total_manpower_imported_sanctioned_post_copy.second_level_id = $second_level_id AND
+total_manpower_imported_sanctioned_post_copy.org_code=$org_code
+GROUP BY total_manpower_imported_sanctioned_post_copy.designation
+ORDER BY total_manpower_imported_sanctioned_post_copy.designation
+";
+$result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>get_sp_second_level:3</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+
+//$total_rows= mysql_num_rows($sanctioned_post_list_result);
+
+echo "<table class=\"table\">";
+while ($data_list = mysql_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td width=\"50%\">" . $data_list['designation'] . " (Staff Id:" . $data_list['staff_id'] . ")</td>";
+    echo "<td>";
+    echo "<button class=\"btn btn-small\" id=\"designation" . $data_list['designation_code'] . "\"type=\"button\">View Sanctioned Posts</button>";
+    echo "</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td colspan=2>";
+    ?>
+    <div id="loading-desig<?php echo $data_list['designation_code']; ?>"  class="alert" style="display: none;"><i class="icon-spinner icon-spin icon-large"></i> Loading Sanctioned Posts...</div>
+    <div id="descri<?php echo $data_list['designation_code']; ?>"></div>
+    <script type="text/javascript">
+        $("#designation<?php echo $data_list['designation_code']; ?>").click(function() {
+            $("#loading-desig<?php echo $data_list['designation_code']; ?>").show();
+            $.ajax({
+                type: "POST",
+                url: "get/get_sp_posts.php",
+                data: {
+                    first_level_id: "<?php echo $first_level_id; ?>",
+                    second_level_id: "<?php echo $data_list['second_level_id']; ?>",
+                    div: "div_2nd_",
+                    loading: "loading-2nd-"
+                },
+                success: function(data) {
+                    $("#loading-desig<?php echo $data_list['designation_code']; ?>").hide();
+                    $('#descri<?php echo $data_list['designation_code']; ?>').html("");
+                    $("#descri<?php echo $data_list['designation_code']; ?>").append(data);
+                }
+            });
+
+        });
+    </script>
+    <?php
+    echo "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
---
+++ get/get_sp_second_level.php
@@ -0,0 +1,74 @@
+<?php
+require_once '../configuration.php';
+
+if (isset($_REQUEST["first_level_id"])) {
+    $first_level_id = mysql_real_escape_string($_REQUEST['first_level_id']);
+//   echo $first_level_id;
+}
+if (isset($_REQUEST["div"])) {
+    $div_id = mysql_real_escape_string($_REQUEST['div']);
+}
+if (isset($_REQUEST["loading"])) {
+    $loading_id = mysql_real_escape_string($_REQUEST['loading']);
+}
+
+$org_code = $_SESSION['org_code'];
+
+$sql = "SELECT
+total_manpower_imported_sanctioned_post_copy.second_level_id,
+total_manpower_imported_sanctioned_post_copy.second_level_name
+FROM total_manpower_imported_sanctioned_post_copy
+WHERE
+total_manpower_imported_sanctioned_post_copy.first_level_id = $first_level_id AND
+total_manpower_imported_sanctioned_post_copy.second_level_name != \"\" AND
+total_manpower_imported_sanctioned_post_copy.org_code=$org_code
+GROUP BY total_manpower_imported_sanctioned_post_copy.second_level_name
+ORDER BY total_manpower_imported_sanctioned_post_copy.second_level_name";
+$result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>get_sp_second_level:3</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+
+//$total_rows= mysql_num_rows($sanctioned_post_list_result);
+//while ($data_list = mysql_fetch_assoc($result)) {
+//    $data[] = array(
+//        "second_level_id" => $data_list['second_level_id'],
+//        "second_level_name" => $data_list['second_level_name']
+//    );
+//}
+
+echo "<table class=\"table\">";
+while ($data_list = mysql_fetch_assoc($result)) {
+    echo "<tr class=\"success\">";
+    echo "<td width=\"50%\">" . $data_list['second_level_name'] . " (Code:" . $data_list['second_level_id'] . ")</td>";
+    echo "<td>";
+    echo "<button class=\"btn btn-small btn-info\" id=\"2nd" . $data_list['second_level_id'] . "\"type=\"button\">View Designations</button>";
+    echo "</td>";
+    echo "</tr>";
+    echo "<tr class=\"success\">";
+    echo "<td colspan=2>";
+    ?>
+    <div id="<?php echo $loading_id . $data_list['second_level_id']; ?>"  class="alert" style="display: none;"><i class="icon-spinner icon-spin icon-large"></i> Loading designation list...</div>
+    <div id="<?php echo $div_id . $data_list['second_level_id']; ?>"></div>
+    <script type="text/javascript">
+        $("#2nd<?php echo $data_list['second_level_id']; ?>").click(function() {
+            $("#<?php echo $loading_id . $data_list['second_level_id']; ?>").show();
+            $.ajax({
+                type: "POST",
+                url: "get/get_sp_designations.php",
+                data: {
+                    first_level_id: "<?php echo $first_level_id; ?>",
+                    second_level_id: "<?php echo $data_list['second_level_id']; ?>"
+                },
+                success: function(data) {
+                    $("#<?php echo $loading_id . $data_list['second_level_id']; ?>").hide();
+                    $('#<?php echo $div_id . $data_list['second_level_id']; ?>').html("");
+                    $("#<?php echo $div_id . $data_list['second_level_id']; ?>").append(data);
+                }
+            });
+
+        });
+    </script>
+    <?php
+    echo "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
