8ff9ad3c99b208ebfbf68d69bcef97441efba352
nasirkhan <nasir8891@gmail.com>
2013-07-22 15:11:29 +0600

    update

--- include/header/header.inc.php
+++ include/header/header.inc.php
@@ -1,5 +1,14 @@
 <?php
 
+// meta data information 
+include_once 'header_meta_info.inc.php';
+
+// css
+include_once 'header_css.inc.inc.php';
+
+// css
+include_once 'header_icons.inc.php';
+
 // google analytics code
 include_once 'header_ga.inc.php';
 ?>
