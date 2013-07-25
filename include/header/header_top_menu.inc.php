0e9bd90d898cf8a0ee3b693d95aedf32f09f009a
nasirkhan <nasir8891@gmail.com>
2013-07-25 22:35:42 +0600

    moved top header menu

---
+++ include/header/header_top_menu.inc.php
@@ -0,0 +1,22 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="./index.php"><?php echo $app_name; ?></a>
+            <div class="nav-collapse collapse">
+                <ul class="nav">
+                    <li class="active">
+                        <a href="./index.php">Home</a>                                
+                    </li>
+                    <li class="">
+                        <a href="http://www.dghs.gov.bd" target="_brank">DGHS Website</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
