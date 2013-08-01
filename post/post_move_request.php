6184ab52fd668aa3466d5d15b1226e2c81c6aa72
nasirkhan <nasir8891@gmail.com>
2013-08-01 14:03:47 +0600

    fix

--- post/post_move_request.php
+++ post/post_move_request.php
@@ -2,6 +2,10 @@
 
 require_once '../configuration.php';
 
+if ($_SESSION['logged'] != true) {
+    header("location:login.php");
+}
+
 $staff_id = mysql_real_escape_string($_POST['staff_id']);
 $sanctioned_post = mysql_real_escape_string($_POST['sanctioned_post']);
 $new_sanctioned_post = mysql_real_escape_string($_POST['new_sanctioned_post']);
