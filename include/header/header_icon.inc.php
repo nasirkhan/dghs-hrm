b31beb52ee49fcfea225269c8ae539c5b99187ba
nasirkhan <nasir8891@gmail.com>
2013-08-25 16:50:47 +0600

    update: header icon

---
+++ include/header/header_icon.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author nasir khan saikat <nasir8891@gmail.com>
+ */
+?>
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/ico/favicon.png">
