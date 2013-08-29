b2c5b2b3c56663a9064573d3a6823aa0ea2ca316
nasirkhan <nasir8891@gmail.com>
2013-08-29 14:34:36 +0600

    resolve conflict

--- include/db_connection.php
+++ include/db_connection.php
@@ -9,7 +9,14 @@
 $dbhost='localhost';	
 $dbname='dghs_hrm_main';	
 $dbuser='root';
-$dbpass='';
+$dbpass='saikat';
+
+
+// remote 
+//$dbhost='localhost';	
+//$dbname='stagedgh_dghs';	
+//$dbuser='stagedgh_hrm';
+//$dbpass='activation';
 
 mysql_select_db($dbname,mysql_connect($dbhost, $dbuser, $dbpass))or die(mysql_errno());
 
