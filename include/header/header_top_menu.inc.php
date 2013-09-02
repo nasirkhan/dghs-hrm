147f6579fbecf74560bf8e3f61c41a98d8699c7f
nasirkhan <nasir8891@gmail.com>
2013-09-02 12:00:21 +0600

    migrate to bootstrap3

--- include/header/header_top_menu.inc.php
+++ include/header/header_top_menu.inc.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once 'configuration.php';
 
 if ($_SESSION['logged'] != true) {
@@ -7,24 +7,24 @@ if ($_SESSION['logged'] != true) {
 ?>
 
 <div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="navbar-inner">
-        <div class="container">
-            <button type="button" class="btn navbar-btn" data-toggle="collapse" data-target=".navbar-collapse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="brand" href="./index.php"><?php echo $app_name; ?></a>
-            <div class="navbar-collapse collapse">
-                <ul class="nav">
-                    <li class="active">
-                        <a href="./index.php">Home</a>                                
-                    </li>
-                    <li class="">
-                        <a href="http://www.dghs.gov.bd" target="_brank">DGHS Website</a>
-                    </li>
-                </ul>
-            </div>
+            <a class="navbar-brand" href="./index.php"><?php echo $app_name; ?></a>
         </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="./index.php">Home</a>
+                </li>
+                <li>
+                    <a href="http://www.dghs.gov.bd" target="_brank">DGHS Website</a>
+                </li>                
+            </ul>
+        </div><!--/.nav-collapse -->
     </div>
 </div>
