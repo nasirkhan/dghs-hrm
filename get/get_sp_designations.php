57f76ae0b992dc7931809a768a32a2713c534261
nasirkhan <nasir8891@gmail.com>
2013-09-19 17:29:33 +0600

    update org_code

--- get/get_sp_designations.php
+++ get/get_sp_designations.php
@@ -70,6 +70,7 @@ while ($data_list = mysql_fetch_assoc($result)) {
                     $("#loading-desig<?php echo $data_list['designation_code']; ?>").hide();
                     $('#descri<?php echo $data_list['designation_code']; ?>').html("");
                     $("#descri<?php echo $data_list['designation_code']; ?>").append(data);
+                    $("#designation<?php echo $data_list['designation_code']; ?>").addClass('hide_content');
                 }
             });
 
