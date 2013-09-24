6641e6e547ddd20db30bba624a5f231238548acd
srajib_dghs <rajib1111@gmail.com>
2013-09-24 13:08:26 +0600

    sanctioned Post page updated

--- sanctioned_post.php
+++ sanctioned_post.php
@@ -104,7 +104,7 @@ if($_SESSION['user_type']=="admin" && $_GET['org_code'] != ""){
                                         <tr>
                                             <th>Sanctioned Post</th>  
 											<th><a href='sanctioned_post_print.php?org_code=<?php echo $org_code;?>'>Print</a></th>
-                                            <th><a href='sanctioned_post_print.php'>All Post</a></th>
+                                         
                                       
 										</tr>
                                     </thead>
