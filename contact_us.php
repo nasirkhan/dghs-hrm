8404cc6c651f86d7de923361158d533688c4ace2
moly4565 <moly4565@yahoo.com>
2013-09-24 15:32:25 +0600

    I did not change bootstrap3. jquery validation cheaked ok. check pls

--- contact_us.php
+++ contact_us.php
@@ -4,88 +4,73 @@
 <head> 
     <title>Contact Us</title>
 	<meta charset="utf-8">    
-<link href="assets/css/bootstrap.css" rel="stylesheet">  
-<script type="text/javascript">
-function validateForm()
-{
-var name=document.forms["form1"]["name"].value;
-var email=document.forms["form1"]["email"].value;
-var orgcode=document.forms["form1"]["orgcode"].value;
-var x=document.forms["form1"]["subject"].value;
-if (name==null || name=="")
-  {
-  alert("Name must be filled out");
-  return false;
-  }
-  if (email==null || email=="")
-  {
-  alert("Email must be filled out");
-  return false;
-  }
-  if (orgcode==null || orgcode=="")
-  {
-  alert("Organization Code must be filled out");
-  return false;
-  }
-}
+	<meta name="description" content="Twitter Bootstrap Version2.0 default form layout example from w3resource.com.">   
+<link href="assets/bootstrap3/css/bootstrap.css" rel="stylesheet" media="screen"> 
 
-</script>
+ <script src="assets/js/jquery.js"></script>
+ <script src="assets/js/bootstrap.min.js"></script>
+ 
+ <script src="assets/jquery Validation/dist/jquery.validate.js"></script>
+ 
 </head>
 <body>
-  <div class=”container”>
-  <div class=”page-header”>
+
+    <div class="container">
+    <div class="page-header" >
                 <h1>Contact Us</h1>
             </div>
-  <form class="form-horizontal" action="send_email.php" method="post" name="form1" id="form1" onSubmit="return validateForm()" >
-                <div class=”control-group”>
-                    <label class=”control-label” for=”input1?>To * </label>
-                    <div class=”controls”>
-					       <input type="text" id="mailto" name="mailto" size="80 px" />
+  <form class="form-horizontal" action="send_email.php" method="post" name="form1" id="form1" >
+                <fieldset >
+                    <div style=" alignment-adjust: central;">
+                    <label class="control-label" for="input1" >To <font color="red"> * </font> </label>
+                    <div class="controls" >
+					    <input type="text" id="mailto" name="mailto" class="form-control" required />
 					</div>
 				</div>
-				 <div class=”control-group”>
-                    <label class=”control-label” for=”input2?>CC</label>
-                    <div class=”controls”>
-					      <input type="text" id="emailcc" name="emailcc" size="80 px"  />
+				
+				 <div class="control-group">
+                    <label class="control-label" for="input2">CC</label>
+                    <div class="controls">
+					      <input type="text" id="emailcc" name="emailcc" class="form-control"  />
 						   
 					</div>
 				</div>
 				
-				 <div class=”control-group”>
-                    <label class=”control-label” for=”input3?>From *</label>
-                    <div class=”controls”>
-					      <input type="text" id="email" name="email" size="80 px"  />
+				 <div class="control-group">
+                    <label class="control-label" for="input3">From <font color="red"> * </font> </label>
+                    <div class="controls">
+					      <input type="text" id="email" name="email" class="form-control" required />
 						   
 					</div>
 				</div>
-				<div class=”control-group”>
-                    <label class=”control-label” for=”input4?>Name *</label>
-                    <div class=”controls”>
-					      <input type="text" id="name" name="name" size="80 px" />
-						   
-					</div>
-				</div>
-				
-				<div class=”control-group”>
-                    <label class=”control-label” for=”input5?>Organization Name(Optional)</label>
-                    <div class=”controls”>
-					      <input type="text" id="orgname" name="orgname" size="80 px" />
+				<div class="control-group">
+                    <label class="control-label" for="input4">Name <font color="red"> * </font></label>
+                    <div class="controls">
+					      <input type="text" id="name" name="name" class="form-control" required />
 						   
 					</div>
 				</div>
 				
-				<div class=”control-group”>
-                    <label class=”control-label” for=”input6?>Organization Code *</label>
-                    <div class=”controls”>
-					      <input type="text" id="orgcode" name="orgcode" size="80 px" />
+				<div class="control-group">
+                    <label class="control-label" for="input5">Organization Name(Optional)</label>
+                    <div class="controls">
+					      <input type="text" id="orgname" name="orgname" class="form-control"/>
 						   
 					</div>
 				</div>
 				
-				<div class=”control-group”>
-                    <label class=”control-label” for=”input7?>Reason *</label>
-                    <div class=”controls”>
-					      <select id="reason" name="reason">
+				<div class="control-group">
+                    <label class="control-label" for="input6">Organization Code <font color="red"> * </font> </label>
+                    <div class="controls">
+					      <input type="text" id="orgcode" name="orgcode"  class="form-control" required/>
+						   
+					</div>
+				</div>
+				
+				<div class="control-group">
+                    <label class="control-label" for="input7">Reason <font color="red"> * </font></label>
+                    <div class="controls">
+					      <select id="reason" name="reason" class="form-control" required>
 						  <option value="Login Problem">Login Problem</option>
 							<option value="Complain">Complain</option>
 							<option value="Suggestion">Suggestion</option>
@@ -96,28 +81,31 @@ if (name==null || name=="")
 					</div>
 				</div>
 				
-				<div class=”control-group”>
-                    <label class=”control-label” for=”input8?>Subject *</label>
-                    <div class=”controls”>
-					      <input type="text" id="subject" name="subject" size="80 px"  />
+				<div class="control-group">
+                    <label class="control-label" for="input8">Subject <font color="red"> * </font></label>
+                    <div class="controls">
+					      <input type="text" id="subject" name="subject" class="form-control" required="Please enter" />
 						   
 					</div>
 				</div>
 				
-				 <div class=”control-group”>
-                    <label class=”control-label” for=”input9?>Message</label>
-                    <div class=”controls”>
-                        <textarea name=”message” id=”input3? rows="10"? cols="80"? class=”span5? ></textarea>
+				 <div class="control-group">
+                    <label class="control-label" for="input9">Message <font color="red"> * </font> </label>
+                    <div class="controls">
+                        <textarea name="message" id="input3" rows="10"? cols="58" class="form-control" required ></textarea>
                     </div>
                 </div>
-				<div class=”form-actions”>
-                    
-                    <button type=”submit” class=”btn btn-primary”>Send</button>
+				<p>
+				<div class="form-actions">      
+                    <button type="submit" class="btn btn-primary btn-large" >Send</button>
+				</p>
                 </div>
-  
+
   </form>
-  
+ 
+  </fieldset>
   </div>
+
 
 </body>
 </html>
