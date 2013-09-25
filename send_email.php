f8c1ce6550515c91b51c91e056e517a21a16a24c
nasirkhan <nasir8891@gmail.com>
2013-09-25 14:16:14 +0600

    format

--- send_email.php
+++ send_email.php
@@ -2,28 +2,28 @@
 <?php
 //error_reporting(E_ALL);
 //ini_set('display_errors','On');
-if ($_POST["email"]<>'') {
-  
+if ($_POST["email"] <> '') {
+
     $EmailSubject = $_POST["subject"];
-	$ToEmail = $_POST["mailto"];
-		
-	$mailheader = "From: ".$_POST["email"]."\r\n";
-    $mailheader .= "Reply-To: ".$_POST["email"]."\r\n";
-	$mailheader .= "CC: ".$_POST["emailcc"]."\r\n";
+    $ToEmail = $_POST["mailto"];
+
+    $mailheader = "From: " . $_POST["email"] . "\r\n";
+    $mailheader .= "Reply-To: " . $_POST["email"] . "\r\n";
+    $mailheader .= "CC: " . $_POST["emailcc"] . "\r\n";
     $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
-    $MESSAGE_BODY .= "Name: ".$_POST["name"]."<br>";
-	$MESSAGE_BODY .= "Organization Name: ".$_POST["orgname"]."<br>";
-	$MESSAGE_BODY .= "Organization Code: ".$_POST["orgcode"]."<br>";
-	$MESSAGE_BODY .= "Reason: ".$_POST["reason"]."<br>";
-	$MESSAGE_BODY .= "Subject: ".$_POST["subject"]."<br>";
-    $MESSAGE_BODY .= "Message: ".$_POST["message"]."";
-	
+    $MESSAGE_BODY .= "Name: " . $_POST["name"] . "<br>";
+    $MESSAGE_BODY .= "Organization Name: " . $_POST["orgname"] . "<br>";
+    $MESSAGE_BODY .= "Organization Code: " . $_POST["orgcode"] . "<br>";
+    $MESSAGE_BODY .= "Reason: " . $_POST["reason"] . "<br>";
+    $MESSAGE_BODY .= "Subject: " . $_POST["subject"] . "<br>";
+    $MESSAGE_BODY .= "Message: " . $_POST["message"] . "";
 
-	
+
+
     //mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
-	mail($ToEmail,$EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
-?>
-Your message was sent
+    mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die("Failure");
+    ?>
+    Your message was sent
 
-<?php
+    <?php
 }
