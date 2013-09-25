6ab38302d7a5f96aa2bb6b6d1a205d730b3667a5
nasirkhan <nasir8891@gmail.com>
2013-09-25 14:16:03 +0600

    update asearch result

--- get/get_search_result.php
+++ get/get_search_result.php
@@ -3,13 +3,18 @@
 //get_search_result
 require_once '../configuration.php';
 
-$type = mysql_real_escape_string($_POST['type']);
-$username = mysql_real_escape_string($_POST['searchUser']);
+$type = mysql_real_escape_string(trim($_POST['type']));
 
-$query_key = mysql_real_escape_string($_POST['searchOrg']);
+$username = mysql_real_escape_string(trim($_POST['searchUser']));
 
-if ($query_key == "" && $username == ""){
-    echo "Please enter an search word.";
+$query_key = mysql_real_escape_string(trim($_POST['searchOrg']));
+
+$query_staff = mysql_real_escape_string(trim($_POST['searchStaff']));
+
+$searchStaffType = mysql_real_escape_string(trim($_POST['searchStaffType']));
+if ($query_key == "" && $username == "" && $query_staff == "") {
+
+    echo "<div class=\"well well-large\"><em>Please Enter a Search Word.</em><div>";
     die();
 }
 
@@ -36,8 +41,7 @@ if ($type == "user") {
     } else {
         echo "\" 0 (Zero) Staff found. \"";
     }
-} 
-else if ($type == "org") {
+} else if ($type == "org") {
     $sql = "SELECT
             organization.org_name,
             organization.org_code
@@ -66,8 +70,7 @@ else if ($type == "org") {
     } else {
         echo "\" 0 (Zero) Organization found. \"";
     }
-} 
-else if ($type == "staff_org") {
+} else if ($type == "staff_org") {
     $sql = "SELECT
             organization.org_name,
             organization.org_code,
@@ -96,5 +99,41 @@ else if ($type == "staff_org") {
     } else {
         echo "\" 0 (Zero) Staff Organization found. \"";
     }
+} else if ($type == "staff") {
+    echo "<div class=\"well\">";
+    $query_string = "";
+
+    if ($searchStaffType == "searchStaffType_name") {
+        $query_string = "staff_name LIKE \"%$query_staff%\"";
+    } else if ($searchStaffType == "searchStaffType_mobile") {
+        $query_string = "contact_no LIKE \"%$query_staff%\"";
+    }
+
+    $sql = "SELECT
+                old_tbl_staff_organization.staff_id,
+                old_tbl_staff_organization.org_code,
+                old_tbl_staff_organization.staff_name,
+                old_tbl_staff_organization.contact_no,
+                old_tbl_staff_organization.birth_date
+            FROM
+                old_tbl_staff_organization
+            WHERE $query_string
+            ORDER BY old_tbl_staff_organization.org_code";
+    $result = mysql_query($sql) or die(mysql_error() . "<p>Code:<b>get_search_result:1</b></p><p><b>Query:</b></p>___<p>$sql</p>");
+    while ($data_list = mysql_fetch_assoc($result)) {
+        echo "<li>";
+        echo "<a href=\"employee.php?staff_id=" . $data_list['staff_id'] . "&org_code=" . $data_list['org_code'] . "\" target=\"_blank\">";
+        echo getOrgNameFormOrgCode($data_list['org_code']) ;
+        echo " - ";
+        echo $data_list['staff_name'] . " (ID:" . $data_list['staff_id'] . ")";
+        echo " - ";
+        echo $data_list['contact_no'];
+        echo " - ";
+        echo $data_list['birth_date'];
+        echo "</a>";
+        echo "</li>";
+    }
+//    echo "$sql";
+    echo "</div>";
 }
 ?>
