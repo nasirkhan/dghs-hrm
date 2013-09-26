21712cbf82563744d31e8f2632d93b6c9317b2f7
nasirkhan <nasir8891@gmail.com>
2013-09-26 15:14:35 +0600

    format

--- upload.php
+++ upload.php
@@ -24,7 +24,6 @@ if ($_SESSION['user_type'] == "admin" && $_GET['org_code'] != "") {
 }
 
 $upload_type = mysql_real_escape_string($_GET['upload']);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,10 +33,10 @@ $upload_type = mysql_real_escape_string($_GET['upload']);
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-		
-         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">   <!-- rajib modified --> 
-		 <link rel="stylesheet" href="library/jQuery-File-Upload-master/css/jquery.fileupload-ui.css">    <!-- rajib modified --> 
-		 
+
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">   <!-- rajib modified --> 
+        <link rel="stylesheet" href="library/jQuery-File-Upload-master/css/jquery.fileupload-ui.css">    <!-- rajib modified --> 
+
         <!-- Le styles -->
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
@@ -128,32 +127,32 @@ $upload_type = mysql_real_escape_string($_GET['upload']);
                                 </table>                            
                             </div>
                         </div>
-                        <?php  if ($upload_type == "photo"){ ?>
-                        <div class="row-fluid">
-                            <div class="span12">
+                        <?php if ($upload_type == "photo") { ?>
+                            <div class="row-fluid">
+                                <div class="span12">
 
-                                <!-- The fileinput-button span is used to style the file input field as button -->
+                                    <!-- The fileinput-button span is used to style the file input field as button -->
                                     <span class="btn btn-success fileinput-button">
-                                      <i class="glyphicon glyphicon-plus"></i>
-                                      <span>Add files...</span>
-                                      <!-- The file input field used as target for the file upload widget -->
-                                      <input id="fileupload" type="file" name="files[]" multiple>
-                                  </span>
-                                  <br>
-                                  <br>
-                                  <!-- The global progress bar -->
-                                  <div id="progress" class="progress">
-                                      <div class="progress-bar progress-bar-success"></div>
-                                  </div>
-                                  <!-- The container for the uploaded files -->
-                                  <div id="files" class="files"></div>
-                                <br>
+                                        <i class="glyphicon glyphicon-plus"></i>
+                                        <span>Add files...</span>
+                                        <!-- The file input field used as target for the file upload widget -->
+                                        <input id="fileupload" type="file" name="files[]" multiple>
+                                    </span>
+                                    <br>
+                                    <br>
+                                    <!-- The global progress bar -->
+                                    <div id="progress" class="progress">
+                                        <div class="progress-bar progress-bar-success"></div>
+                                    </div>
+                                    <!-- The container for the uploaded files -->
+                                    <div id="files" class="files"></div>
+                                    <br>
 
+                                </div>
                             </div>
-                        </div>
 
                         <?php } ?>
-                        
+
                     </section>
 
                 </div>
@@ -181,7 +180,7 @@ $upload_type = mysql_real_escape_string($_GET['upload']);
 
         <script src="assets/js/application.js"></script>
 
-        
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
         <script src="library/jQuery-File-Upload-master/js/vendor/jquery.ui.widget.js"></script>
@@ -205,102 +204,102 @@ $upload_type = mysql_real_escape_string($_GET['upload']);
         <script src="library/jQuery-File-Upload-master/js/jquery.fileupload-video.js"></script>
         <!-- The File Upload validation plugin -->
         <script src="library/jQuery-File-Upload-master/js/jquery.fileupload-validate.js"></script>
-		<!--<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>-->
-       <script>
-/*jslint unparam: true, regexp: true */
-/*global window, $ */
-$(function () {
-    'use strict';
-    // Change this to the location of your server-side upload handler:
-		var url = window.location.hostname === 'http://gov.vd.com' ? '/fileupload/' : 'uploads/',
-        uploadButton = $('<button/>')
-            .addClass('btn btn-primary')
-            .prop('disabled', true)
-            .text('Processing...')
-            .on('click', function () {
-                var $this = $(this),
-                    data = $this.data();
-                $this
-                    .off('click')
-                    .text('Uploading..')
-                    .on('click', function () {
+                <!--<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>-->
+        <script>
+            /*jslint unparam: true, regexp: true */
+            /*global window, $ */
+            $(function() {
+                'use strict';
+                // Change this to the location of your server-side upload handler:
+                var url = window.location.hostname === 'http://gov.vd.com' ? '/fileupload/' : 'uploads/',
+                        uploadButton = $('<button/>')
+                        .addClass('btn btn-primary')
+                        .prop('disabled', true)
+                        .text('Processing...')
+                        .on('click', function() {
+                    var $this = $(this),
+                            data = $this.data();
+                    $this
+                            .off('click')
+                            .text('Uploading..')
+                            .on('click', function() {
                         $this.remove();
                         data.abort();
                     });
-                data.submit().always(function () {
-                    $this.remove();
+                    data.submit().always(function() {
+                        $this.remove();
+                    });
                 });
+                $('#fileupload').fileupload({
+                    url: url,
+                    dataType: 'json',
+                    autoUpload: false,
+                    acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+                    maxFileSize: 5000000, // 5 MB
+                    // Enable image resizing, except for Android and Opera,
+                    // which actually support image resizing, but fail to
+                    // send Blob objects via XHR requests:
+                    disableImageResize: /Android(?!.*Chrome)|Opera/
+                            .test(window.navigator.userAgent),
+                    previewMaxWidth: 100,
+                    previewMaxHeight: 100,
+                    previewCrop: true
+                }).on('fileuploadadd', function(e, data) {
+                    data.context = $('<div/>').appendTo('#files');
+                    $.each(data.files, function(index, file) {
+                        var node = $('<p/>')
+                                .append($('<span/>').text(file.name));
+                        if (!index) {
+                            node
+                                    .append('<br>')
+                                    .append(uploadButton.clone(true).data(data));
+                        }
+                        node.appendTo(data.context);
+                    });
+                }).on('fileuploadprocessalways', function(e, data) {
+                    var index = data.index,
+                            file = data.files[index],
+                            node = $(data.context.children()[index]);
+                    if (file.preview) {
+                        node
+                                .prepend('<br>')
+                                .prepend(file.preview);
+                    }
+                    if (file.error) {
+                        node
+                                .append('<br>')
+                                .append(file.error);
+                    }
+                    if (index + 1 === data.files.length) {
+                        data.context.find('button')
+                                .text('Upload')
+                                .prop('disabled', !!data.files.error);
+                    }
+                }).on('fileuploadprogressall', function(e, data) {
+                    var progress = parseInt(data.loaded / data.total * 100, 10);
+                    $('#progress .progress-bar').css(
+                            'width',
+                            progress + '%'
+                            );
+                }).on('fileuploaddone', function(e, data) {
+                    $.each(data.result.files, function(index, file) {
+                        var link = $('<a>')
+                                .attr('target', '_blank')
+                                .prop('href', file.url);
+                        $(data.context.children()[index])
+                                .wrap(link);
+                    });
+                }).on('fileuploadfail', function(e, data) {
+                    $.each(data.result.files, function(index, file) {
+                        var error = $('<span/>').text(file.error);
+                        $(data.context.children()[index])
+                                .append('<br>')
+                                .append(error);
+                    });
+                }).prop('disabled', !$.support.fileInput)
+                        .parent().addClass($.support.fileInput ? undefined : 'disabled');
             });
-    $('#fileupload').fileupload({
-        url: url,
-        dataType: 'json',
-        autoUpload: false,
-        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-        maxFileSize: 5000000, // 5 MB
-        // Enable image resizing, except for Android and Opera,
-        // which actually support image resizing, but fail to
-        // send Blob objects via XHR requests:
-        disableImageResize: /Android(?!.*Chrome)|Opera/
-            .test(window.navigator.userAgent),
-        previewMaxWidth: 100,
-        previewMaxHeight: 100,
-        previewCrop: true
-    }).on('fileuploadadd', function (e, data) {
-        data.context = $('<div/>').appendTo('#files');
-        $.each(data.files, function (index, file) {
-            var node = $('<p/>')
-                    .append($('<span/>').text(file.name));
-            if (!index) {
-                node
-                    .append('<br>')
-                    .append(uploadButton.clone(true).data(data));
-            }
-            node.appendTo(data.context);
-        });
-    }).on('fileuploadprocessalways', function (e, data) {
-        var index = data.index,
-            file = data.files[index],
-            node = $(data.context.children()[index]);
-        if (file.preview) {
-            node
-                .prepend('<br>')
-                .prepend(file.preview);
-        }
-        if (file.error) {
-            node
-                .append('<br>')
-                .append(file.error);
-        }
-        if (index + 1 === data.files.length) {
-            data.context.find('button')
-                .text('Upload')
-                .prop('disabled', !!data.files.error);
-        }
-    }).on('fileuploadprogressall', function (e, data) {
-        var progress = parseInt(data.loaded / data.total * 100, 10);
-        $('#progress .progress-bar').css(
-            'width',
-            progress + '%'
-        );
-    }).on('fileuploaddone', function (e, data) {
-        $.each(data.result.files, function (index, file) {
-            var link = $('<a>')
-                .attr('target', '_blank')
-                .prop('href', file.url);
-            $(data.context.children()[index])
-                .wrap(link);
-        });
-    }).on('fileuploadfail', function (e, data) {
-        $.each(data.result.files, function (index, file) {
-            var error = $('<span/>').text(file.error);
-            $(data.context.children()[index])
-                .append('<br>')
-                .append(error);
-        });
-    }).prop('disabled', !$.support.fileInput)
-        .parent().addClass($.support.fileInput ? undefined : 'disabled');
-});
-</script>
-</body> 
+        </script>
+    </body> 
 
 </html>
