6a53ddc68e074b1269cafdfd85790c9207db0664
nasirkhan <nasir8891@gmail.com>
2013-10-01 17:15:13 +0600

    offline page

---
+++ offline.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+        <title>HRM System of the Ministry of Health and Family Welfare</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="assets/bootstrap3/css/bootstrap.min.css" rel="stylesheet">
+
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="assets/bootstrap3/js/html5shiv.js"></script>
+          <script src="assets/bootstrap3/js/respond.min.js"></script>
+        <![endif]-->
+        <style type="text/css">
+            /* Sticky footer styles
+-------------------------------------------------- */
+
+            html,
+            body {
+                height: 100%;
+                /* The html and body elements cannot have any padding or margin. */
+            }
+
+            /* Wrapper for page content to push down footer */
+            #wrap {
+                min-height: 100%;
+                height: auto !important;
+                height: 100%;
+                /* Negative indent footer by its height */
+                margin: 0 auto -60px;
+                /* Pad bottom by footer height */
+                padding: 0 0 60px;
+            }
+
+            /* Set the fixed height of the footer here */
+            #footer {
+                height: 60px;
+                background-color: #f5f5f5;
+            }
+
+
+            /* Custom page CSS
+            -------------------------------------------------- */
+            /* Not required for template or sticky footer method. */
+
+            .container {
+                width: auto;
+                max-width: 680px;
+                padding: 0 15px;
+            }
+            .container .credit {
+                margin: 20px 0;
+            }
+        </style>
+        <?php include_once 'include/header/header_ga.inc.php'; ?>
+    </head>
+
+    <body>
+
+        <div id="wrap">
+
+            <!-- Begin page content -->
+            <div class="container">
+                <!--                <div class="page-header">
+                                    
+                                </div>-->
+                <div class="jumbotron">
+                    <h1>HRM System Offline</h1>
+                    <p class="lead">
+                        Welcome to <em>Ministry of Health and Family Welfare</em> HRM system.
+
+                    <p class="alert alert-warning">At this moment we are upgrading this HRM system. You will be able to use this software as soon as we finish the maintenance.</p>
+                    </p>                    
+                </div>
+            </div>
+        </div>
+
+        <div id="footer">
+            <div class="container">
+                <p class="text-muted credit">
+
+                <ul class="list-inline">
+                    <li><a href="http://dghs.gov.bd">DGHS Website</a></li>
+                    <li><a href="contact_us.php">Contact Us</a></li>
+                </ul>
+
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
