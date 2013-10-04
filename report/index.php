3e8759348f3727cce3b7b77a4dddcc2807ee3145
nasirkhan <nasir8891@gmail.com>
2013-10-04 12:17:04 +0600

    report page updated

--- report/index.php
+++ report/index.php
@@ -96,7 +96,70 @@ if ($_SESSION['user_type'] == "admin" && $_GET['org_code'] != "") {
                     <section id="report">
 
                         <div class="row">
-
+                            <?php
+                            $sql = "SELECT
+                                            id,
+                                            designation,
+                                            designation_code,
+                                            COUNT(*) AS sp_count 
+                                    FROM
+                                            total_manpower_imported_sanctioned_post_copy
+                                    WHERE
+                                            org_code = $org_code
+                                    GROUP BY 
+                                            designation
+                                    ORDER BY
+                                            designation";
+                            $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>sql:2</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+                            $total_sanctioned_post = mysql_num_rows($result);
+                            
+                            
+                            $total_sanctioned_post_count_sum = 0;
+                            $total_sanctioned_post_existing_sum = 0;
+                            ?>
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Designation</th>
+                                        <th>Total Sanctioned Post(s)</th>
+                                        <th>Filled up Post(s)</th>
+                                        <th>Vacant Post(s)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php 
+                                    while ($row = mysql_fetch_assoc($result)) :
+                                        $sql= "SELECT
+                                                        designation,
+                                                        designation_code,
+                                                        COUNT(*) AS existing_total_count
+                                                FROM
+                                                        total_manpower_imported_sanctioned_post_copy
+                                                WHERE
+                                                        org_code = $org_code
+                                                AND designation_code = " . $row['designation_code'] . "
+                                                AND staff_id_2 > 0;";
+                                                    $r = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>sql:2</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+                                            $a = mysql_fetch_assoc($r);
+                                            
+                                            $total_sanctioned_post_count_sum += $row['sp_count'];
+                                            $total_sanctioned_post_existing_sum += $a['existing_total_count'];
+                                        ?>
+                                    <tr>
+                                        <td><?php echo $row['designation']; ?></td>
+                                        <td><?php echo $row['sp_count']; ?></td>
+                                        <td><?php echo $a['existing_total_count']; ?></td>
+                                        <td><?php echo $row['sp_count']-$a['existing_total_count']; ?></td>
+                                    </tr>
+                                    <?php endwhile; ?>
+                                    <tr class="info">
+                                        <td><strong>Summary</strong></td>
+                                        <td><strong><?php echo $total_sanctioned_post_count_sum; ?></strong></td>
+                                        <td><strong><?php echo $total_sanctioned_post_existing_sum; ?></strong></td>
+                                        <td><strong><?php echo $total_sanctioned_post_count_sum-$total_sanctioned_post_existing_sum; ?></string></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
 
                     </section>
