4c0a34d5e4b642e0384f847ef27990ed910946bb
Nasir Khan Saikat <nasir8891@gmail.com>
2013-10-24 17:05:43 +0600

    added male female sum

--- report/index.php
+++ report/index.php
@@ -116,6 +116,8 @@ if ($_SESSION['user_type'] == "admin" && $_GET['org_code'] != "") {
                             
                             $total_sanctioned_post_count_sum = 0;
                             $total_sanctioned_post_existing_sum = 0;
+                            $total_existing_male_sum = 0;
+                            $total_existing_female_sum = 0;
                             ?>
                             <table class="table table-striped">
                                 <thead>
@@ -123,6 +125,8 @@ if ($_SESSION['user_type'] == "admin" && $_GET['org_code'] != "") {
                                         <th>Designation</th>
                                         <th>Total Sanctioned Post(s)</th>
                                         <th>Filled up Post(s)</th>
+                                        <th>Total Male</th>
+                                        <th>Total Female</th>
                                         <th>Vacant Post(s)</th>
                                     </tr>
                                 </thead>
@@ -141,21 +145,45 @@ if ($_SESSION['user_type'] == "admin" && $_GET['org_code'] != "") {
                                                 AND staff_id_2 > 0;";
                                                     $r = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>sql:2</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
                                             $a = mysql_fetch_assoc($r);
+                                            $existing_total_count = $a['existing_total_count'];
                                             
+                                            $sql= "SELECT
+                                                        total_manpower_imported_sanctioned_post_copy.designation,
+                                                        total_manpower_imported_sanctioned_post_copy.designation_code,
+                                                        COUNT(*) AS existing_male_count
+                                                FROM
+                                                        total_manpower_imported_sanctioned_post_copy
+                                                LEFT JOIN old_tbl_staff_organization ON old_tbl_staff_organization.staff_id = total_manpower_imported_sanctioned_post_copy.staff_id_2
+                                                WHERE
+                                                        total_manpower_imported_sanctioned_post_copy.org_code = $org_code
+                                                AND total_manpower_imported_sanctioned_post_copy.designation_code = " . $row['designation_code'] . "
+                                                AND total_manpower_imported_sanctioned_post_copy.staff_id_2 > 0
+                                                AND old_tbl_staff_organization.sex=1";
+                                                    $r = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>sql:2</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+                                            $a = mysql_fetch_assoc($r);
+                                            $existing_male_count = $a['existing_male_count'];
+                                            
+                                            $existing_female_count = $existing_total_count-$existing_male_count;
                                             $total_sanctioned_post_count_sum += $row['sp_count'];
-                                            $total_sanctioned_post_existing_sum += $a['existing_total_count'];
+                                            $total_sanctioned_post_existing_sum += $existing_total_count;
+                                            $total_existing_male_sum += $existing_male_count;
+                                            $total_existing_female_sum += $existing_female_count;
                                         ?>
                                     <tr>
                                         <td><?php echo $row['designation']; ?></td>
                                         <td><?php echo $row['sp_count']; ?></td>
-                                        <td><?php echo $a['existing_total_count']; ?></td>
-                                        <td><?php echo $row['sp_count']-$a['existing_total_count']; ?></td>
+                                        <td><?php echo $existing_total_count; ?></td>
+                                        <td><?php echo $existing_male_count; ?></td>
+                                        <td><?php echo $existing_female_count; ?></td>
+                                        <td><?php echo $row['sp_count']-$existing_total_count; ?></td>
                                     </tr>
                                     <?php endwhile; ?>
                                     <tr class="info">
                                         <td><strong>Summary</strong></td>
                                         <td><strong><?php echo $total_sanctioned_post_count_sum; ?></strong></td>
                                         <td><strong><?php echo $total_sanctioned_post_existing_sum; ?></strong></td>
+                                        <td><strong><?php echo $total_existing_male_sum; ?></strong></td>
+                                        <td><strong><?php echo $total_existing_female_sum; ?></strong></td>
                                         <td><strong><?php echo $total_sanctioned_post_count_sum-$total_sanctioned_post_existing_sum; ?></string></td>
                                     </tr>
                                 </tbody>
