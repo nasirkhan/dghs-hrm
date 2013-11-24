d5c7942630bf0b3b35da968729f316cb2586fff2
Nasir Khan Saikat <nasir8891@gmail.com>
2013-11-24 18:50:37 +0600

    new report page

---
+++ report/report_monthly_update.php
@@ -0,0 +1,343 @@
+<?php
+require_once '../configuration.php';
+
+if ($_SESSION['logged'] != true) {
+    header("location:../login.php");
+}
+
+// assign values from session array
+$org_code = $_SESSION['org_code'];
+$org_name = $_SESSION['org_name'];
+$org_type_name = $_SESSION['org_type_name'];
+
+$echoAdminInfo = "";
+
+// assign values admin users
+if ($_SESSION['user_type'] == "admin" && $_GET['org_code'] != "") {
+    $org_code = (int) mysql_real_escape_string($_GET['org_code']);
+    $org_name = getOrgNameFormOrgCode($org_code);
+    $org_type_name = getOrgTypeNameFormOrgCode($org_code);
+    $echoAdminInfo = " | Administrator";
+    $isAdmin = TRUE;
+}
+
+$div_id = (int) mysql_real_escape_string(trim($_REQUEST['admin_division']));
+$dis_id = (int) mysql_real_escape_string(trim($_REQUEST['admin_district']));
+$upa_id = (int) mysql_real_escape_string(trim($_REQUEST['admin_upazila']));
+$agency_code = (int) mysql_real_escape_string(trim($_REQUEST['org_agency']));
+$type_code = (int) mysql_real_escape_string(trim($_REQUEST['org_type']));
+$form_submit = (int) mysql_real_escape_string(trim($_REQUEST['form_submit']));
+
+if ($form_submit == 1 && isset($_REQUEST['form_submit'])) {
+
+    /*
+     * 
+     * query builder to get the organizatino list
+     */
+    $query_string = "";
+    if ($div_id > 0 || $dis_id > 0 || $upa_id > 0 || $agency_code > 0 || $type_code > 0) {
+        $query_string .= " WHERE ";
+
+        if ($agency_code > 0) {
+            $query_string .= "organization.agency_code = $agency_code";
+        }
+        if ($upa_id > 0) {
+            if ($agency_code > 0) {
+                $query_string .= " AND ";
+            }
+            $query_string .= "organization.upazila_id = $upa_id";
+        }
+        if ($dis_id > 0) {
+            if ($upa_id > 0 || $agency_code > 0) {
+                $query_string .= " AND ";
+            }
+            $query_string .= "organization.district_id = $dis_id";
+        }
+        if ($div_id > 0) {
+            if ($dis_id > 0 || $upa_id > 0 || $agency_code > 0) {
+                $query_string .= " AND ";
+            }
+            $query_string .= "organization.division_id = $div_id";
+        }
+        if ($type_code > 0) {
+            if ($div_id > 0 || $dis_id > 0 || $upa_id > 0 || $agency_code > 0) {
+                $query_string .= " AND ";
+            }
+            $query_string .= "organization.org_type_code = $type_code";
+        }
+    }
+
+    $sql = "SELECT
+                    organization.org_type_name,
+                    organization.org_type_code,
+                    COUNT(*) AS total_count
+            FROM
+                    organization
+            $query_string
+            GROUP BY
+                    organization.org_type_code";
+    $sql .= " ORDER BY org_type_name";
+    $org_type_summary_result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>get_org_type_summary:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+//    echo "$sql";
+    
+    
+    date_default_timezone_set('Asia/Dhaka');
+    $current_month = date("n");
+    
+    if ($div_id > 0 || $dis_id > 0 || $upa_id > 0 || $agency_code > 0 || $type_code > 0){
+        $query_string .= "AND organization.monthly_update = $current_month";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $org_name . " | " . $app_name; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="Nasir Khan Saikat(nasir8891@gmail.com)">
+
+        <!-- Le styles -->
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="../library/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <link href="../assets/css/style.css" rel="stylesheet">
+        <link href="../assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="assets/js/html5shiv.js"></script>
+        <![endif]-->
+
+        <!-- Le fav and touch icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+        <link rel="shortcut icon" href="../assets/ico/favicon.png">
+
+        <!--Google analytics code-->
+        <?php include_once '../include/header/header_ga.inc.php'; ?>
+    </head>
+
+    <body data-spy="scroll" data-target=".bs-docs-sidebar">
+
+        <!-- Top navigation bar
+        ================================================== -->
+        <?php include_once '../include/header/header_top_menu.inc.php'; ?>
+
+        <!-- Subhead
+        ================================================== -->
+        <header class="jumbotron subhead" id="overview">
+            <div class="container">
+                <h1><?php echo $org_name . $echoAdminInfo; ?></h1>
+                <p class="lead"><?php echo "$org_type_name"; ?></p>
+            </div>
+        </header>
+
+
+        <div class="container">
+
+            <!-- Docs nav
+            ================================================== -->
+            <div class="row">
+                <div class="span3 bs-docs-sidebar">
+                    <ul class="nav nav-list bs-docs-sidenav">
+                        <?php if ($_SESSION['user_type'] == "admin"): ?>
+                            <li><a href="../admin_home.php?org_code=<?php echo $org_code; ?>"><i class="icon-chevron-right"></i><i class="icon-qrcode"></i> Admin Homepage</a>
+                            <?php endif; ?>
+                            <?php
+                            $active_menu = "report/index";
+                            include_once '../include/left_menu_report_page.php';
+                            ?>
+                    </ul>
+                </div>
+                <div class="span9">
+                    <!-- info area
+                    ================================================== -->
+                    <section id="report">
+
+                        <div class="row">
+                            <div class="">
+                                <form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+                                    <p class="lead">Get the summation of the staffs</p>
+                                    <div class="control-group">
+                                        <select id="org_agency" name="org_agency">
+                                            <option value="0">Select Agency</option>
+                                            <?php
+                                            $sql = "SELECT
+                                                    org_agency_code.org_agency_code,
+                                                    org_agency_code.org_agency_name
+                                                FROM
+                                                    org_agency_code
+                                                ORDER BY
+                                                    org_agency_code.org_agency_code";
+                                            $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>loadorg_agency:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+
+                                            while ($rows = mysql_fetch_assoc($result)) {
+                                                echo "<option value=\"" . $rows['org_agency_code'] . "\">" . $rows['org_agency_name'] . "</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                    <div class="control-group">
+                                        <select id="admin_division" name="admin_division">
+                                            <option value="0">Select Division</option>
+                                            <?php
+                                            /**
+                                             * @todo change old_visision_id to division_bbs_code
+                                             */
+                                            $sql = "SELECT admin_division.division_name, admin_division.old_division_id FROM admin_division";
+                                            $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>loadDivision:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+
+                                            while ($rows = mysql_fetch_assoc($result)) {
+                                                echo "<option value=\"" . $rows['old_division_id'] . "\">" . $rows['division_name'] . "</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                        <select id="admin_district" name="admin_district">
+                                            <option value="0">Select District</option>                                        
+                                        </select>
+                                        <select id="admin_upazila" name="admin_upazila">
+                                            <option value="0">Select Upazila</option>                                        
+                                        </select>
+                                    </div>
+
+
+                                    <div class="control-group">
+
+                                        <select id="org_type" name="org_type">
+                                            <option value="0">Select Org Type</option>
+                                            <?php
+                                            $sql = "SELECT
+                                                            org_type.org_type_code,
+                                                            org_type.org_type_name
+                                                        FROM
+                                                            org_type
+                                                        ORDER BY
+                                                            org_type.org_type_name ASC";
+                                            $result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>loadorg_type:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+
+                                            while ($rows = mysql_fetch_assoc($result)) {
+                                                echo "<option value=\"" . $rows['org_type_code'] . "\">" . $rows['org_type_name'] . "</option>";
+                                            }
+                                            ?>
+                                        </select>
+
+
+
+                                    </div>
+                                    <input name="form_submit" value="1" type="hidden" />
+                                    <div class="control-group">
+                                        <button id="btn_show_org_list" type="submit" class="btn btn-info">Show Report</button>
+
+                                        <a id="loading_content" href="#" class="btn btn-info disabled" style="display:none;"><i class="icon-spinner icon-spin icon-large"></i> Loading content...</a>
+                                    </div>  
+                                </form>
+                            </div>
+
+                            <?php if ($form_submit == 1 && isset($_REQUEST['form_submit'])) :
+                                if (mysql_num_rows($org_type_summary_result)):
+                                    ?>
+                                    <table class="table table-striped table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <td><strong>Organization Type</strong></td>
+                                                <td><strong>Total Number</strong></td>
+                                                <td><strong>Updated HRM Data in Current Month</strong></td>
+                                                <td><strong>Did not updated HRM Data in Current Month</strong></td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php while ($row = mysql_fetch_assoc($org_type_summary_result)): ?>
+                                                <tr>
+                                                    <td><?php echo $row['org_type_name']; ?></td>
+                                                    <td><?php echo $row['total_count']; ?></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                </tr>
+                                            <?php endwhile; ?>
+                                        </tbody>
+                                    </table>
+
+                                <?php endif; ?>
+                            <?php endif; ?>
+                        </div>
+
+                    </section>
+
+                </div>
+            </div>
+
+        </div>
+
+
+
+        <!-- Footer
+        ================================================== -->
+        <?php include_once '../include/footer/footer_menu.inc.php'; ?>
+
+
+
+        <!-- Le javascript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <!--<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>-->
+        <script src="../assets/js/jquery.js"></script>
+        <script src="../assets/js/bootstrap.min.js"></script>
+
+        <script src="../assets/js/holder/holder.js"></script>
+        <script src="../assets/js/google-code-prettify/prettify.js"></script>
+
+        <script src="../assets/js/application.js"></script>
+
+        <script type="text/javascript">
+            // load division
+            $('#admin_division').change(function() {
+                $("#loading_content").show();
+                var div_id = $('#admin_division').val();
+                $.ajax({
+                    type: "POST",
+                    url: '../get/get_district_list.php',
+                    data: {div_id: div_id},
+                    dataType: 'json',
+                    success: function(data)
+                    {
+                        $("#loading_content").hide();
+                        var admin_district = document.getElementById('admin_district');
+                        admin_district.options.length = 0;
+                        for (var i = 0; i < data.length; i++) {
+                            var d = data[i];
+                            admin_district.options.add(new Option(d.text, d.value));
+                        }
+                    }
+                });
+            });
+
+            // load district 
+            $('#admin_district').change(function() {
+                var dis_id = $('#admin_district').val();
+                $("#loading_content").show();
+                $.ajax({
+                    type: "POST",
+                    url: '../get/get_upazila_list.php',
+                    data: {dis_id: dis_id},
+                    dataType: 'json',
+                    success: function(data)
+                    {
+                        $("#loading_content").hide();
+                        var admin_upazila = document.getElementById('admin_upazila');
+                        admin_upazila.options.length = 0;
+                        for (var i = 0; i < data.length; i++) {
+                            var d = data[i];
+                            admin_upazila.options.add(new Option(d.text, d.value));
+                        }
+                    }
+                });
+            });
+        </script>
+
+
+    </body>
+</html>
