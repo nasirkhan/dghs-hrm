9fc3e82387f5e257e2c5c1675255e06ce6507e61
nasirkhan <nasir8891@gmail.com>
2013-12-31 16:01:38 +0600

    db select

--- api_config.php
+++ api_config.php
@@ -15,6 +15,7 @@ $dbname='dghs_hrm_main';
 $dbuser='root';
 $dbpass='';
 
+mysql_select_db($dbname,mysql_connect($dbhost, $dbuser, $dbpass))or die(mysql_errno());
 
 require_once 'include/functions_app_specific.php';
 require_once 'include/functions_generic.php';
