3cd5019480c3dfd6e1808acdde4ba27ce469398b
Raihan Sikder <raihan.act@gmail.com>
2014-01-06 11:45:00 +0600

    left menu update, - To view old menu put parameterized url  ?show_old_menu=true

--- include/left_menu_admin.php
+++ include/left_menu_admin.php
@@ -4,20 +4,20 @@
     <li><a href="">Admin Home</a></li>
     <li><a href="">Organizations</a></li>
     <ul>
-        <li><a href="">Search</a></li>
-        <li><a href="">Add</a></li>
+        <li><a href="search.php?type=org">Search</a></li>
+        <li><a href="#">Add</a></li>
         <li><a href="">Organizations Awaiting Approval</a></li>
         <li><a href="">All Employee</a></li>
     </ul>
     <li><a href="">Reports</a></li>
-    <li><a href="">Settings</a></li>  
+    <li><a href="setting.php?org_code=<?= $org_code?>">Settings</a></li>  
     <ul>
-        <li><a href="">Designations</a></li>
-        <li><a href="">Class</a></li>
-        <li><a href="">Payscale</a></li>
-        <li><a href="">Type of Post</a></li>
-        <li><a href="">First Level Names</a></li>
-        <li><a href="">Second Level Name</a></li>        
+        <li><a href="#">Designations</a></li>
+        <li><a href="#">Class</a></li>
+        <li><a href="#">Payscale</a></li>
+        <li><a href="#">Type of Post</a></li>
+        <li><a href="#">First Level Names</a></li>
+        <li><a href="#">Second Level Name</a></li>        
     </ul>    
 </ul>
 
