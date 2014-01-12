c18982fef78a769ebc6dee12d7fe365786aa57d5
nasirkhan <nasir8891@gmail.com>
2014-01-12 16:39:17 +0600

    add new organization

--- get/get_org_division_name.php
+++ get/get_org_division_name.php
@@ -2,7 +2,7 @@
 require_once '../configuration.php';
 
 $sql = "SELECT `division_name`, `division_bbs_code` FROM `admin_division` ORDER BY division_name";
-$result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>checkPasswordIsCorrect:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
+$result = mysql_query($sql) or die(mysql_error() . "<br /><br />Code:<b>get_org_division_name:1</b><br /><br /><b>Query:</b><br />___<br />$sql<br />");
 
 $data = array();
 while ($row = mysql_fetch_array($result)) {
