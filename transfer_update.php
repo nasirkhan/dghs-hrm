abf5b4ad6d8420accc0cbd4a5d501b601f1bd55a
nasirkhan <nasir8891@gmail.com>
2014-04-15 16:15:16 +0600

    fix

--- transfer_update.php
+++ transfer_update.php
@@ -30,17 +30,19 @@ if (isset($_REQUEST['order_type'])) {
     if ($order_type == "order") {
         $sql = "SELECT * FROM `transfer_queue` WHERE order_creater_by LIKE \"$user_name\" and `status` LIKE \"pending\" ORDER BY `serial`";
         $listed_result = mysql_query($sql) or die(mysql_error() . "<p>Code:getStaffDetails:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+        
+        $transfer_memo_no = mysql_real_escape_string(trim($_POST['memo_no']));
+        $transfer_memo_date = mysql_real_escape_string(trim($_POST['memo_date']));
+        $transfer_comment = mysql_real_escape_string(trim($_POST['comment']));
+        
+        $sql = "UPDATE `transfer_queue` SET `memo_no`='$transfer_memo_no', `memo_date`='$transfer_memo_date', `comment`='$transfer_comment' WHERE order_creater_by LIKE \"$user_name\" and `status` LIKE \"pending\" ORDER BY `serial`";
+        $r = mysql_query($sql) or die(mysql_error() . "<p>Code:getStaffDetails:2<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+        
     } else if ($order_type == "order_submit") {
-//        echo "paisi";
-        $order_memo_no = mysql_real_escape_string(trim($_POST['order_memo_no']));
-        $order_memo_date = mysql_real_escape_string(trim($_POST['order_memo_date']));
-        $order_comment = mysql_real_escape_string(trim($_POST['order_comment']));
-                       
         $sql = "SELECT * FROM `transfer_queue` WHERE order_creater_by LIKE \"$user_name\" and `status` LIKE \"pending\" ORDER BY `serial`";
         $listed_result = mysql_query($sql) or die(mysql_error() . "<p>Code:getStaffDetails:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
-//        echo "<pre>$sql</pre>";
-//        die();
-        while ($row = mysql_fetch_assoc($listed_result)){
+        
+        while ($row = mysql_fetch_assoc($listed_result)) {
             $sql = "INSERT INTO `transfer_staff` (
                             `request_submitted_by`,
                             `staff_id`,
@@ -65,52 +67,50 @@ if (isset($_REQUEST['order_type'])) {
                             '" . $row['to_designation_code'] . "',
                             '" . $row['to_designation_code'] . "',
                             '" . $row['to_org_code'] . "',
-                            '" . $_POST['order_memo_no'] . "',
-                            '" . $_POST['order_memo_date'] . "',
-                            '" . $_POST['order_memo_comment'] . "',
+                            '" . $row['memo_no'] . "',
+                            '" . $row['memo_date'] . "',
+                            '" . $row['comment'] . "',
                             '$user_name'
                     )";
-            
+
             $r = mysql_query($sql) or die(mysql_error() . "<p>Code:2<br /><br /><b>Query:</b><br />___<br />$sql</p>");
-            
-            
+
+
             $sql = "UPDATE `transfer_queue` SET `status`='order' WHERE (`id`='" . $row['id'] . "')";
-            $r = mysql_query($sql) or die(mysql_error() . "<p>Code:3<br /><br /><b>Query:</b><br />___<br />$sql</p>");            
+            $r = mysql_query($sql) or die(mysql_error() . "<p>Code:3<br /><br /><b>Query:</b><br />___<br />$sql</p>");
         }
         header('location:transfer.php');
-    } else if ($order_type == "release"){
+    } else if ($order_type == "release") {
         $staff_id = mysql_real_escape_string(trim($_REQUEST['staff_id']));
-        
+
         $sql = "SELECT * FROM `transfer_staff` WHERE staff_id = '$staff_id' AND `status` LIKE 'order' AND active LIKE '1'";
-        $staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:4<br /><br /><b>Query:</b><br />___<br />$sql</p>");        
-        
+        $staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:4<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
 //        $data = mysql_fetch_assoc($staff_result);
 //        print_r($sql);
-    } else if ($order_type == "release_submit"){
+    } else if ($order_type == "release_submit") {
         $transfer_id = mysql_real_escape_string(trim($_REQUEST['transfer_id']));
-        
+
         $sql = "UPDATE `transfer_staff` SET `release_approved_by`='$user_name', `status`='release', `updated_by`='$user_name' WHERE (`id`='$transfer_id')";
-        $staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:5<br /><br /><b>Query:</b><br />___<br />$sql</p>");        
-        
-        header('location:transfer_staff.php');
-    } else if ($order_type == "join"){
+        $staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:5<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+        header("location:transfer_staff.php?org_code=$org_code");
+    } else if ($order_type == "join") {
         $staff_id = mysql_real_escape_string(trim($_REQUEST['staff_id']));
-        
+
         $sql = "SELECT * FROM `transfer_staff` WHERE staff_id = '$staff_id' AND `status` LIKE 'release' AND active LIKE '1'";
-        $staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:4<br /><br /><b>Query:</b><br />___<br />$sql</p>");        
-        
+        $staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:4<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
 //        $data = mysql_fetch_assoc($staff_result);
 //        print_r($sql);
-    } else if ($order_type == "join_submit"){
+    } else if ($order_type == "join_submit") {
         $transfer_id = mysql_real_escape_string(trim($_REQUEST['transfer_id']));
-        
+
         $sql = "UPDATE `transfer_staff` SET `release_approved_by`='$user_name', `status`='join', `updated_by`='$user_name' WHERE (`id`='$transfer_id')";
-        $staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:5<br /><br /><b>Query:</b><br />___<br />$sql</p>");        
-        
-        header('location:transfer_staff.php');
+        $staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:5<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+        header("location:transfer_staff.php?org_code=$org_code");
     }
-    
-    
 } else {
     header('location:transfer.php');
 }
@@ -132,7 +132,7 @@ if (isset($_REQUEST['order_type'])) {
         <div class="hidden-print">
             <?php include_once 'include/header/header_top_menu.inc.php'; ?>
         </div>
-        
+
 
         <!-- Subhead
         ================================================== -->
@@ -162,12 +162,9 @@ if (isset($_REQUEST['order_type'])) {
                     ================================================== -->
                     <section id="search_main">
                         <?php
-                        if ($_POST['order_type'] == "order"):
+                        if ($_REQUEST['order_type'] == "order"):
                             $transfer_top_header = "Bnagladesh Govt.";
-                            $transfer_memo_no = mysql_real_escape_string(trim($_POST['memo_no']));
-                            $transfer_memo_date = mysql_real_escape_string(trim($_POST['memo_date']));
                             $transfer_top_intro = "BCS info.....alskdasd asdada asdajdlas alaks caslnasslaj as'las .";
-                            $transfer_comment = mysql_real_escape_string(trim($_POST['comment']));
                             $transfer_signed_by = "Name :lasdj <br> Phone: 123456789 <br> email: abc@def.gh";
                             ?>
 
@@ -245,18 +242,27 @@ if (isset($_REQUEST['order_type'])) {
                                     </p>
                                 </div>
                             </div>
+                            <div class="hidden-print">
+                                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+                                    <input type="hidden" name="transfer_id" value="<?php echo $data['id']; ?>" />
+                                    <input type="hidden" name="order_type" value="order_submit" />
+                                    <input type="hidden" name="order_memo_no" value="<?php echo mysql_real_escape_string(trim($_POST['order_memo_no']));; ?>" />
+                                    <input type="hidden" name="order_memo_date" value="<?php echo mysql_real_escape_string(trim($_POST['order_memo_date']));; ?>" />
+                                    <textarea name="order_comment" style="display:none;"></textarea>
+                                    <button type="submit" class="btn btn-success">Confirm Order</button>
+                                </form>
+                            </div>
                         <?php elseif ($_REQUEST['order_type'] == "release"):
                             $data = mysql_fetch_assoc($staff_result);
-                            
+
                             $transfer_id = $data['id'];
                             $transfer_top_header = "Bnagladesh Govt.";
                             $transfer_memo_no = $data['memo_no'];
-                            $transfer_memo_date = $data['memo_date']; 
+                            $transfer_memo_date = $data['memo_date'];
                             $transfer_top_intro = "BCS info.....alskdasd asdada asdajdlas alaks caslnasslaj as'las .";
                             $transfer_comment = $data['comment'];
                             $transfer_signed_by_1 = "Name : <br> Designation: <br> Phone:  <br> Email: ";
                             $transfer_signed_by_2 = "Name : <br> Designation: <br> Phone:  <br> Email: ";
-                                    
                             ?>
 
                             <div class="row-fluid">
@@ -338,19 +344,24 @@ if (isset($_REQUEST['order_type'])) {
                                     </p>
                                 </div>
                             </div>                        
-                        
+                            <div class="hidden-print">
+                                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+                                    <input type="hidden" name="transfer_id" value="<?php echo $data['id']; ?>" />
+                                    <input type="hidden" name="order_type" value="release_submit" />
+                                    <button type="submit" class="btn btn-success">Confirm Release</button>
+                                </form>
+                            </div>
                         <?php elseif ($_REQUEST['order_type'] == "join"):
                             $data = mysql_fetch_assoc($staff_result);
-                            
+
                             $transfer_id = $data['id'];
                             $transfer_top_header = "Bnagladesh Govt.";
                             $transfer_memo_no = $data['memo_no'];
-                            $transfer_memo_date = $data['memo_date']; 
+                            $transfer_memo_date = $data['memo_date'];
                             $transfer_top_intro = "BCS info.....alskdasd asdada asdajdlas alaks caslnasslaj as'las .";
                             $transfer_comment = $data['comment'];
                             $transfer_signed_by_1 = "Name : <br> Designation: <br> Phone:  <br> Email: ";
                             $transfer_signed_by_2 = "Name : <br> Designation: <br> Phone:  <br> Email: ";
-                                    
                             ?>
 
                             <div class="row-fluid">
@@ -432,15 +443,16 @@ if (isset($_REQUEST['order_type'])) {
                                     </p>
                                 </div>
                             </div>
+                            <div class="hidden-print">
+                                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+                                    <input type="hidden" name="transfer_id" value="<?php echo $data['id']; ?>" />
+                                    <input type="hidden" name="order_type" value="join_submit" />
+                                    <button type="submit" class="btn btn-success">Confirm Join</button>
+                                </form>
+                            </div>
                         <?php endif; ?>
-                        
-                        <div class="hidden-print">
-                            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-                                <input type="hidden" name="transfer_id" value="<?php echo $data['id']; ?>" />
-                                <input type="hidden" name="order_type" value="join_submit" />
-                                <button type="submit" class="btn btn-success">Confirm Join</button>
-                            </form>
-                        </div>
+
+
                     </section>
                 </div>
             </div>
