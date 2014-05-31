d24a7615a7a277b7973384f8de765d8140d501b8
raihan_sikder <raihan.act@gmail.com>
2014-05-31 15:53:28 +0600

    updated grid

--- report_org_list_1.1.php
+++ report_org_list_1.1.php
@@ -71,7 +71,7 @@ if (isset($_REQUEST['submit'])) {
   /**
    * Show fields
    */
-  //$showFields = getTableFieldNames('organization');
+  $TableFields = getTableFieldNames('organization');
 
   if (count($_REQUEST['f'])) {
     $showFields = $_REQUEST['f'];
@@ -475,71 +475,78 @@ if (isset($_REQUEST['submit'])) {
             <tr>
               <?php
               foreach ($showFields as $fieldName) {
-                ?>
-                <td id="<?= $fieldName ?>"><strong><a href="#" title="<?= $fieldName ?>">
-                      <?php
-                      if (strlen($fieldNameAlias[$fieldName])) {
-                        if ($replaceUnderScoreWithSpace) {
-                          $fieldName = str_replace('_', ' ', $fieldNameAlias[$fieldName]);
+                if (in_array($fieldName, $TableFields)) {
+                  ?>
+                  <td id="<?= $fieldName ?>"><strong><a href="#" title="<?= $fieldName ?>">
+                        <?php
+                        if (strlen($fieldNameAlias[$fieldName])) {
+                          if ($replaceUnderScoreWithSpace) {
+                            $fieldName = str_replace('_', ' ', $fieldNameAlias[$fieldName]);
+                          } else {
+                            $fieldName = $fieldNameAlias[$fieldName];
+                          }
                         } else {
-                          $fieldName = $fieldNameAlias[$fieldName];
+                          if ($replaceUnderScoreWithSpace) {
+                            $fieldName = str_replace('_', ' ', $fieldName);
+                          }
                         }
-                      } else {
-                        if ($replaceUnderScoreWithSpace) {
-                          $fieldName = str_replace('_', ' ', $fieldName);
-                        }
-                      }
-                      echo $fieldName;
-                      ?>
-                    </a>
-                  </strong>
-                </td>
-                <?php
+                        echo $fieldName;
+                        ?>
+                      </a>
+                    </strong>
+                  </td>
+                  <?php
+                }
               }
               if (strlen($countField)) {
-                echo "<td>Total</td>";
+                echo "<td><b>Total</b></td>";
               }
               ?>
 
             </tr>
           </thead>
           <tbody>
-            <?php while ($data = mysql_fetch_assoc($result)) { ?>
+            <?php
+            $subTotal = 0;
+            while ($data = mysql_fetch_assoc($result)) {
+              ?>
               <tr>
                 <?php
                 $totalFields = count($showFields);
                 $filledFields = 0;
                 foreach ($showFields as $fieldName) {
-                  //if (in_array($fieldName, $showFields)) {
-                  if ($DBvalidation == TRUE) {
-                    $tdClass = "";
-                    if (!strlen($data[$fieldName])) {
-                      $filledFields++;
-                      $tdClass = "bgRed";
-                    } else {
+                  if (in_array($fieldName, $TableFields)) {
+                    if ($DBvalidation == TRUE) {
+                      $tdClass = "";
+                      if (!strlen($data[$fieldName])) {
+                        $filledFields++;
+                        $tdClass = "bgRed";
+                      } else {
 
-                      //  }
+                        //  }
+                      }
                     }
+                    ?>
+                    <td class="<?= $tdClass ?>"><?php echo $data[$fieldName]; ?></td>
+                    <?php
                   }
-                  ?>
-                  <td class="<?= $tdClass ?>"><?php echo $data[$fieldName]; ?></td>
-                  <?php
                 }
                 if (strlen($countField)) {
+                  $subTotal+=$data['total'];
                   echo "<td>" . $data['total'] . "</td>";
                 }
                 ?>
 
               </tr>
-            <?php } ?>
+  <?php } ?>
           </tbody>
         </table>
-      <?php } ?>
+<?php } ?>
     </div>
 
     <!-- Footer
     ================================================== -->
-    <?php include_once 'include/footer/footer.inc.php'; ?>
+<?php include_once 'include/footer/footer.inc.php'; ?>
 
     <script type="text/javascript">
       // load division
@@ -584,11 +591,11 @@ if (isset($_REQUEST['submit'])) {
           }
         });
       });</script>
-              <script type="text/javascript">
-              var tableToExcel = (function() {
-                      var uri = 'data:application/vnd.ms-excel;base64,'
-              , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
-                                , base64 = function(s){
+    <script type="text/javascript">
+      var tableToExcel = (function() {
+        var uri = 'data:application/vnd.ms-excel;base64,'
+                , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+                , base64 = function(s) {
                   return window.btoa(unescape(encodeURIComponent(s)))
                 }
         , format = function(s, c) {
@@ -606,25 +613,25 @@ if (isset($_REQUEST['submit'])) {
     </script>
 
     <script type="text/javascript">
-                                        $('table#datatable').dataTable({
-                                //"bJQueryUI": true,
-                                "bPaginate": false,
-                                        "sPaginationType": "full_numbers",
-                                        "aaSorting": [[0, "desc"]],
-                                        "iDisplayLength": 25,
-                                        "bStateSave": true,
-                                        "bInfo": true,
-                                        "bProcessing": true,
-                                        "dom": 'T<"clear">lfrtip',
-                                        "tableTools": {
-                                        "sSwfPath": "assets/datatable/TableTools/media/swf/copy_csv_xls_pdf.swf"
-                                        }
-                                });</script>
+      $('table#datatable').dataTable({
+        //"bJQueryUI": true,
+        "bPaginate": false,
+        "sPaginationType": "full_numbers",
+        "aaSorting": [[0, "desc"]],
+        "iDisplayLength": 25,
+        "bStateSave": true,
+        "bInfo": true,
+        "bProcessing": true,
+        "dom": 'T<"clear">lfrtip',
+        "tableTools": {
+          "sSwfPath": "assets/datatable/TableTools/media/swf/copy_csv_xls_pdf.swf"
+        }
+      });</script>
     <script type="text/javascript">
-                              $('.multiselect').multiselect({
-                      includeSelectAllOption: true,
-                              maxHeight: 200,
-                      });</script>
+      $('.multiselect').multiselect({
+        includeSelectAllOption: true,
+        maxHeight: 200,
+      });</script>
   </body>
 </html>
 
