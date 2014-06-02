cdfb608caae99396d3ae716bf52e93611c37cef6
nasirkhan <nasir8891@gmail.com>
2014-06-02 16:39:50 +0600

    restructure

--- scripts/update_data/fill_sanctioned_post_table.php
+++ scripts/update_data/fill_sanctioned_post_table.php
@@ -20,6 +20,7 @@ require_once '../../include/functions_generic.php';
 
 //  Limit the maximum execution time
 set_time_limit(123456);
+$start_time = microtime(true);
 
 /** ----------------------------------------------------------------------------
  *  
@@ -28,172 +29,230 @@ set_time_limit(123456);
  * -----------------------------------------------------------------------------
  */
 echo "<pre>";
+$show_query = TRUE;
+
+/**
+ * sanctioned_post_designation array
+ */
+$code = 'designation_code';
+$value = 'designation';
+$table = 'sanctioned_post_designation';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+$designation = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $designation [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+
+
+/**
+ * bangladesh_professional_category_name array
+ */
+$code = 'bangladesh_professional_category_code';
+$value = 'bangladesh_professional_category_name';
+$table = 'sanctioned_post_bangladesh_professional_category';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+$bangladesh_professional_category_name = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $bangladesh_professional_category_name [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+
+/**
+ * who_occupation_group_name array
+ */
+$code = 'who_health_professional_group_code';
+$value = 'who_health_professional_group_name';
+$table = 'sanctioned_post_who_health_professional_group';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+$who_occupation_group_name = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $who_occupation_group_name [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+/**
+ * first_level_name array
+ */
+$code = 'id';
+$value = 'first_level_name';
+$table = 'sanctioned_post_first_level';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+$first_level_name = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $first_level_name [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+$first_level_name['0'] = "";
+/**
+ * first_level_code array
+ */
+$code = 'id';
+$value = 'first_level_code';
+$table = 'sanctioned_post_first_level';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+$first_level_code = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $first_level_code [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+$first_level_code['0'] = "0";
+
+/**
+ * second_level_name array
+ */
+$code = 'id';
+$value = 'second_level_name';
+$table = 'sanctioned_post_second_level';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+$second_level_name = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $second_level_name [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+$second_level_name['0'] = "";
+
+/**
+ * second_level_code array
+ */
+$code = 'id';
+$value = 'second_level_code';
+$table = 'sanctioned_post_second_level';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+$second_level_code = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $second_level_code [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+$second_level_code['0'] = "0";
+
+
 
 $sql = "SELECT * FROM `total_manpower_imported_sanctioned_post_copy` ORDER BY org_code limit 5";
 $sp_result = mysql_query($sql) or die(mysql_error() . "<p>Code:getAllSanctionedPost:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
 
-$row_count = 0; 
-while ($sp_data = mysql_fetch_assoc($sp_result)){
+$row_count = 0;
+while ($sp_data = mysql_fetch_assoc($sp_result)) {
     $row_count++;
-    echo "<br /><br />------------------<br />$row_count | " 
-            . $sp_data['org_code'] . " | " 
-            . $sp_data['org_name'] . " | "
-            . $sp_data['id'] . ""
-            . "<br />--- ";
-    
-    
+   
     // get org loaction data of this sanctioned post
     $org_location_data = getOrgInfoOfSanctionedPost($sp_data['org_code']);
-    
+
+    $query_string = "";
+
     // update org_name
     $code_field_name = 'org_code';
     $value_field_name = 'org_name';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['org_name'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
+
     // update org_type_name
     $code_field_name = 'org_code';
     $value_field_name = 'org_type_name';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['org_type_name'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
+
     // update org_type_code
     $code_field_name = 'org_code';
     $value_field_name = 'org_type_code';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['org_type_code'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
+
     // update division_name
     $code_field_name = 'org_code';
     $value_field_name = 'division_name';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['division_name'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
+
     // update division_code
     $code_field_name = 'org_code';
     $value_field_name = 'division_code';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['division_code'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
+
+
     // update district_name
     $code_field_name = 'org_code';
     $value_field_name = 'district_name';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['district_name'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
+
+
     // update district_code
     $code_field_name = 'org_code';
     $value_field_name = 'district_code';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['district_code'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
+
+
     // update upazila_thana_name
     $code_field_name = 'org_code';
     $value_field_name = 'upazila_name';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['upazila_thana_name'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
+
     // update upazila_thana_code
     $code_field_name = 'org_code';
     $value_field_name = 'upazila_code';
-    $code = $sp_data[$code_field_name];    
+    $code = $sp_data[$code_field_name];
     $value = $org_location_data['upazila_thana_code'];
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
-    // update designation
-    $code_field_name = 'designation_code';
-    $value_field_name = 'designation';
-    $code = $sp_data[$code_field_name];    
-    $value = mysql_real_escape_string(trim(getDesignationNameformCode($code)));
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
-    // update bangladesh_professional_category_name
-    $code_field_name = 'bangladesh_professional_category_code';
-    $value_field_name = 'bangladesh_professional_category_name';
-    $code = $sp_data[$code_field_name];    
-    $value = mysql_real_escape_string(trim(getBangladeshProfessionalStaffCategoryFromCode($code)));
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
-    // update who_occupation_group_name
-    $code_field_name = 'who_occupation_group_code';
-    $value_field_name = 'who_occupation_group_name';
-    $code = $sp_data[$code_field_name];    
-    $value = mysql_real_escape_string(trim(getWhoProfessionalGroupNameFromCode($code)));
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    // get first level info
-    $first_level_info = getFirstLevelInfoFromId($sp_data['first_level_id']);
-    
-    // update first_level_name
-    $code_field_name = 'first_level_id';
-    $value_field_name = 'first_level_name';
-    $code = $sp_data[$code_field_name];    
-    $value = mysql_real_escape_string(trim($first_level_info[$value_field_name]));
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
-    // update first_level_code
-    $code_field_name = 'first_level_id';
-    $value_field_name = 'first_level_code';
-    $code = $sp_data[$code_field_name];    
-    $value = mysql_real_escape_string(trim($first_level_info[$value_field_name]));
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    // get second level info
-    $second_level_info = getSecondtLevelInfoFromId($sp_data['second_level_id']);
-    
-    // update second_level_name
-    $code_field_name = 'second_level_id';
-    $value_field_name = 'second_level_name';
-    $code = $sp_data[$code_field_name];    
-    $value = mysql_real_escape_string(trim($second_level_info[$value_field_name]));
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    
-    // update second_level_code
-    $code_field_name = 'second_level_id';
-    $value_field_name = 'second_level_code';
-    $code = $sp_data[$code_field_name];    
-    $value = mysql_real_escape_string(trim($second_level_info[$value_field_name]));
-    $show_query = TRUE;
-    update_sp_data($code_field_name, $code, $value_field_name, $value, $show_query);
-    
-    // sanctioned post group code needed to be updated.
-}
+    $query_string .= "`$value_field_name`=\"" . $value . "\",";
 
+
+    $query_string .= "`designation`=\"" . $designation[$sp_data['designation_code']] . "\",";
+    $query_string .= "`bangladesh_professional_category_name`=\"" . $bangladesh_professional_category_name[$sp_data['bangladesh_professional_category_code']] . "\",";
+    $query_string .= "`who_occupation_group_name`=\"" . $who_occupation_group_name[$sp_data['who_occupation_group_code']] . "\",";
+    $query_string .= "`first_level_name`=\"" . $first_level_name[$sp_data['first_level_id']] . "\",";
+    $query_string .= "`first_level_code`=\"" . $first_level_code[$sp_data['first_level_id']] . "\",";
+    $query_string .= "`second_level_name`=\"" . $second_level_name[$sp_data['second_level_id']] . "\",";
+    $query_string .= "`second_level_code`=\"" . $second_level_code[$sp_data['second_level_id']] . "\" ";
+
+    /**
+     * ------------------------------------------------------
+     * // sanctioned post group code needed to be updated.
+     * ------------------------------------------------------
+     */
+    /**
+     * 
+     * update query 
+     * 
+     * *****************************************************
+     */    
+    echo "\n\n\r------------------\n$row_count | "
+    . $sp_data['org_code'] . " | "
+    . $sp_data['org_name'] . " | "
+    . $sp_data['id'] . ""
+    . "\n--- ";
+    $sql = "UPDATE `total_manpower_imported_sanctioned_post_copy` SET "
+            . " $query_string "
+            . "WHERE (`id`=\"" . $sp_data['id'] . "\",)";
+//    $result = mysql_query($sql) or die(mysql_error() . "<p>update_$value_field_name:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+    if ($show_query) {
+        echo "\n\r | $sql";
+    }
+}
 
 /** * ***************************************************************************
  *  
@@ -201,9 +260,8 @@ while ($sp_data = mysql_fetch_assoc($sp_result)){
  * 
  * *****************************************************************************
  */
-
-function getOrgInfoOfSanctionedPost($org_code){
-    if (!$org_code > 0){
+function getOrgInfoOfSanctionedPost($org_code) {
+    if (!$org_code > 0) {
         return FALSE;
     }
     $sql = "SELECT
@@ -222,58 +280,23 @@ function getOrgInfoOfSanctionedPost($org_code){
             FROM
                     `organization`";
     $org_result = mysql_query($sql) or die(mysql_error() . "<p>Code:getOrgLocationInfo:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
-    
+
     $data = mysql_fetch_assoc($org_result);
-    
-    return $data;
-}
-
-function update_sp_data ($code_field_name, $code, $value_field_name, $value, $show_query){
-    if (!$code > 0){
-        return FALSE;
-    }
-    $sql = "UPDATE `total_manpower_imported_sanctioned_post_copy` SET `$value_field_name`=\"$value\" WHERE (`$code_field_name`='$code')";
-    $result = mysql_query($sql) or die(mysql_error() . "<p>update_$value_field_name:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
-
-    if ($show_query) {
-        echo "<br />$value_field_name | $sql";
-    }
-}
-
-function getFirstLevelInfoFromId($code) {
-    if (!$code > 0) {
-        $data = ['first_level_code' => '0', 'first_level_name' => '0'];
-        return $data;
-    }
-
-    $sql = "SELECT
-                    *
-            FROM
-                    sanctioned_post_first_level
-            WHERE
-                    id = '$code'";
-    $result = mysql_query($sql) or die(mysql_error() . "<p><b>Code:getFirstLevelInfoFromId:1</p><p>Query:</b></p>___<p>$sql</p>");
-
-    $data = mysql_fetch_assoc($result);
 
     return $data;
 }
 
-function getSecondtLevelInfoFromId($code) {
-    if (!$code > 0) {
-        $data = ['second_level_code' => '0', 'second_level_name' => '0'];
-        return $data;
-    }
+/**
+ * 
+ * 
+ * END
+ */
+$time_end = microtime(true);
 
-    $sql = "SELECT
-                    *
-            FROM
-                    `sanctioned_post_second_level`
-            WHERE
-                    id = '$code'";
-    $result = mysql_query($sql) or die(mysql_error() . "<p><b>Code:getSecondtLevelInfoFromId:1</p><p>Query:</b></p>___<p>$sql</p>");
-
-    $data = mysql_fetch_assoc($result);
-
-    return $data;
-}
+//dividing with 60 will give the execution time in minutes other wise seconds
+$execution_time = ($time_end - $start_time);
+//$execution_time = ($time_end - $start_time);
+//execution time of the script
+echo "<br /><br /><br /> "
+        . "------------------------------------------"
+        . "<br /><b>Total Execution Time: $execution_time Second(s)</b>";
