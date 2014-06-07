e47e1bb4c5dd172907a621d5fabb01de721613e8
nasirkhan <nasir8891@gmail.com>
2014-06-07 12:37:51 +0600

    update

--- scripts/update_data/fill_staff_table.php
+++ scripts/update_data/fill_staff_table.php
@@ -7,7 +7,7 @@
 $dbhost = 'localhost';
 $dbname = 'dghs_hrm_main';
 $dbuser = 'root';
-$dbpass = '';
+$dbpass = 'root';
 
 mysql_select_db($dbname, mysql_connect($dbhost, $dbuser, $dbpass)) or die(mysql_error());
 mysql_query("SET CHARACTER SET utf8");
@@ -88,12 +88,13 @@ $sanctioned_post_designation = array();
 while ($data = mysql_fetch_assoc($r)) {
     $sanctioned_post_designation [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
 }
+//print_r($sanctioned_post_designation);
 
 /**
  * sanctioned_post_designation_name array
  */
 $code = 'id';	
-$value = 'designation_name';
+$value = 'designation';
 $table = 'sanctioned_post_designation';
 $sql = "SELECT $code, $value FROM `$table`";
 $r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:1</b><br />___<br />$sql</p>");
@@ -123,7 +124,7 @@ while ($data = mysql_fetch_assoc($r)) {
  * staff_tribal array
  */
 $code = 'tribal_id';	
-$value = 'tribal_name';
+$value = 'tribal_value';
 $table = 'staff_tribal';
 $sql = "SELECT $code, $value FROM `$table`";
 $r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:1</b><br />___<br />$sql</p>");
@@ -198,38 +199,170 @@ while ($data = mysql_fetch_assoc($r)) {
     $staff_job_posting [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
 }
 
+/**
+ * staff_working_status array
+ */
+$code = 'working_status_id';	
+$value = 'working_status_name';
+$table = 'staff_working_status';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:1</b><br />___<br />$sql</p>");
+
+$staff_working_status = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $staff_working_status [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+
+
+/**
+ * staff_draw_salaray_place array
+ */
+$code = 'draw_salary_id';	
+$value = 'draw_salaray_place';
+$table = 'staff_draw_salaray_place';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:1</b><br />___<br />$sql</p>");
+
+$staff_draw_salaray_place = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $staff_draw_salaray_place [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+
+
+/**
+ * staff_sex array
+ */
+$code = 'sex_type_id';	
+$value = 'sex_name';
+$table = 'staff_sex';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:1</b><br />___<br />$sql</p>");
+
+$staff_sex = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $staff_sex [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+
+
+/**
+ * staff_marital_status array
+ */
+$code = 'marital_status_id';	
+$value = 'marital_status';
+$table = 'staff_marital_status';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:1</b><br />___<br />$sql</p>");
+
+$staff_marital_status = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $staff_marital_status [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+
+
+/**
+ * staff_religious_group array
+ */
+$code = 'religious_group_id';	
+$value = 'religious_group_name';
+$table = 'staff_religious_group';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:1</b><br />___<br />$sql</p>");
+
+$staff_religious_group = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $staff_religious_group [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+
+
+
+/**
+ * staff_profesional_discipline array
+ */
+$code = 'discipline_id';	
+$value = 'discipline_name';
+$table = 'staff_profesional_discipline';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:1</b><br />___<br />$sql</p>");
+
+$staff_profesional_discipline = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $staff_profesional_discipline [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
+
+
+/**
+ * staff_educational_qualification array
+ */
+$code = 'educational_qualifiaction_Id';	
+$value = 'educational_qualification';
+$table = 'staff_educational_qualification';
+$sql = "SELECT $code, $value FROM `$table`";
+$r = mysql_query($sql) or die(mysql_error() . "<p>Code:1<br /><br /><b>Query:staff_educational_qualification</b><br />___<br />$sql</p>");
+
+$staff_educational_qualification = array();
+
+while ($data = mysql_fetch_assoc($r)) {
+    $staff_educational_qualification [mysql_real_escape_string(trim($data[$code]))] = mysql_real_escape_string(trim($data[$value]));
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/** * ***************************************************************************
- *  
- * Function definitions 
+/** 
+ * -----------------------------------------------------------------------------
  * 
- * *****************************************************************************
+ * Staff table update 
+ * 
+ * -----------------------------------------------------------------------------
  */
 
-function update_staff_data ($code_field_name, $code, $value_field_name, $value, $show_query){
-    if (!$code > 0){
-        return FALSE;
-    }
-    $sql = "UPDATE `old_tbl_staff_organization` SET `$value_field_name`=\"$value\" WHERE (`$code_field_name`='$code')";
+$sql = "SELECT * FROM `old_tbl_staff_organization`";
+$staff_result = mysql_query($sql) or die(mysql_error() . "<p>Code:getAllSanctionedPost:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
+
+$show_query = TRUE;
+$query_string = "";
+$row_count = 0;
+
+while ($data = mysql_fetch_assoc($staff_result)) {
+    $row_count++;
+    
+    $query_string .= "`designation`=\"" . $designation[$sp_data['designation_code']] . "\",";
+    
+    
+    
+    $sql = "UPDATE `old_tbl_staff_organization` SET "
+            . " $query_string "
+            . "WHERE (`staff_id`=\"" . $sp_data['id'] . "\")";
+    $sql = "UPDATE `old_tbl_staff_organization` "
+            . "SET `staff_posting_name` = \"" . $staff_job_posting[$data['staff_posting']] . "\", "
+            . "`staff_job_class_name` = \"" . $staff_job_class[$data['staff_job_class']] . "\", "
+            . "`staff_professional_category_name` = \"" . $staff_professional_category_type[$data['staff_professional_category']] . "\", "
+            . "`designation_code` = \"" . $sanctioned_post_designation[$data['designation_id']] . "\", "
+            . "`designation_name` = \"" . $sanctioned_post_designation_name[$data['designation_id']] . "\", "
+            . "`freedom_fighter_name` = \"" . $staff_freedom_fighter[$data['freedom_fighter_id']] . "\", "
+            . "`tribal_name` = \"" . $staff_tribal[$data['tribal_id']] . "\", "
+            . "`post_type_name` = \"" . $staff_post_type[$data['post_type_id']] . "\", "
+            . "`draw_type_name` = \"" . $staff_draw_salaray_place[$data['draw_type_id']] . "\", "
+            . "`designation_type_name` = \"" . $staff_designation_type[$data['designation_type_id']] . "\", "
+            . "`job_posting_name` = \"" . $staff_job_posting[$data['job_posting_id']] . "\", "
+            . "`working_status_name` = \"" . $staff_working_status[$data['working_status_id']] . "\", "
+            . "`draw_salary_name` = \"" . $staff_draw_salaray_place[$data['draw_salary_id']] . "\", "
+            . "`sex_name` = \"" . $staff_sex[$data['sex']] . "\", "
+            . "`marital_status_name` = \"" . $staff_marital_status[$data['marital_status']] . "\", "
+            . "`religion_name` = \"" . $staff_religious_group[$data['religion']] . "\", "
+            . "`professional_discipline_of_current_designation_name` = \"" . $staff_profesional_discipline[$data['professional_discipline_of_current_designation']] . "\", "
+            . "`type_of_educational_qualification_name` = \"" . $staff_educational_qualification[$data['type_of_educational_qualification']] . "\" "
+            . "WHERE  "
+            . "(`staff_id` = '" . $data['staff_id'] . "')";
     $result = mysql_query($sql) or die(mysql_error() . "<p>update_$value_field_name:1<br /><br /><b>Query:</b><br />___<br />$sql</p>");
 
     if ($show_query) {
-        echo "<br />$value_field_name | $sql";
+        echo "\n\r$row_count | Staff ID:" . $data['staff_id'] . " | " . $data['staff_name'] . " \n\r | $sql";
     }
 }
