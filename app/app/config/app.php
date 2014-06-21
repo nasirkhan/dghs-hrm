07b29aedbef5d27ffef15375909deba4b9dc830c
nasirkhan <nasir8891@gmail.com>
2014-06-21 19:31:49 +0600

    included way/generators

--- app/app/config/app.php
+++ app/app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
                 'Cartalyst\Sentry\SentryServiceProvider',
+                'Way\Generators\GeneratorsServiceProvider'
 
 	),
 
