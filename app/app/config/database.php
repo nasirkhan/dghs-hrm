c01f7c97500ac7071924829ac3697b693d6a6884
nasirkhan <nasir8891@gmail.com>
2014-06-21 18:48:51 +0600

    update config

--- app/app/config/database.php
+++ app/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'dghs_hrm_main_l',
+			'username'  => 'root',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
