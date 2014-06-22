b47cab60b37ae84f572313456366c5304be53660
nasirkhan <nasir8891@gmail.com>
2014-06-22 14:54:28 +0600

    +

--- app/app/routes.php
+++ app/app/routes.php
@@ -15,3 +15,33 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+/*
+  |--------------------------------------------------------------------------
+  | Authentication and Authorization Routes
+  |--------------------------------------------------------------------------
+ */
+Route::group(array('prefix' => 'auth'), function() {
+
+    # Login
+    Route::get('signin', array('as' => 'signin', 'uses' => 'AuthController@getSignin'));
+    Route::post('signin', 'AuthController@postSignin');
+
+    # Register
+    Route::get('signup', array('as' => 'signup', 'uses' => 'AuthController@getSignup'));
+    Route::post('signup', 'AuthController@postSignup');
+
+    # Account Activation
+    Route::get('activate/{activationCode}', array('as' => 'activate', 'uses' => 'AuthController@getActivate'));
+
+    # Forgot Password
+    Route::get('forgot-password', array('as' => 'forgot-password', 'uses' => 'AuthController@getForgotPassword'));
+    Route::post('forgot-password', 'AuthController@postForgotPassword');
+
+    # Forgot Password Confirmation
+    Route::get('forgot-password/{passwordResetCode}', array('as' => 'forgot-password-confirm', 'uses' => 'AuthController@getForgotPasswordConfirm'));
+    Route::post('forgot-password/{passwordResetCode}', 'AuthController@postForgotPasswordConfirm');
+
+    # Logout
+    Route::get('logout', array('as' => 'logout', 'uses' => 'AuthController@getLogout'));
+});
