5e17ff40b7e3bb36c1b75958add0afd2add5e289
nasirkhan <nasir8891@gmail.com>
2014-07-22 15:02:42 +0600

    update config

--- scripts/update_data/fill_designation_table.php
+++ scripts/update_data/fill_designation_table.php
@@ -4,15 +4,10 @@
  * Database connection configuration
  *
  */
-$dbhost = 'localhost';
-$dbname = 'dghs_hrm_main';
-$dbuser = 'root';
-$dbpass = '';
-
-mysql_select_db($dbname, mysql_connect($dbhost, $dbuser, $dbpass)) or die(mysql_error());
-mysql_query("SET CHARACTER SET utf8");
-mysql_query("SET SESSION collation_connection ='utf8_general_ci'");
-
+/**
+ * config
+ */
+require_once './config.php';
 
 //  Limit the maximum execution time
 set_time_limit(18000);
